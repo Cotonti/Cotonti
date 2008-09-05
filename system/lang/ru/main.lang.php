8e7246c24bfc2e0125e215131ccbabf04d824048
Vladimir Sibirov <trustmaster@kodigy.com>
2008-09-05 09:59:25 +0000

    A fix in redirect urls, a fix in langfile

--- system/lang/ru/main.lang.php
+++ system/lang/ru/main.lang.php
@@ -457,7 +457,7 @@ $L['com_guests'] = 'гость(ей)';
 
 /* ====== Header ====== */
 
-$L['hea_privatemessages'] = '<img src=\'skins/classic/img/newmail.gif\' title=\'pm\'/ alt=\'pm\'/>';
+$L['hea_privatemessages'] = 'новых сообщений';
 $L['hea_noprivatemessages'] = 'Личные сообщения';
 $L['hea_youareloggedas'] = 'Вы вошли как';
 $L['hea_youarenotlogged'] = 'Вы не вошли.';
