251b1b91a6d81b195be5918046ee201d5e0c4907
Ozan Ciga <ozan.ciga@gmail.com>
2008-11-17 22:29:36 +0000

    Miscalculation fix for revision numbering.

--- docs/sql/updates/sql_r153.sql
+++ docs/sql/updates/sql_r153.sql
@@ -1,4 +1,4 @@
-/* r151 Maintenance mode */
+/* r153 Maintenance mode */
 INSERT INTO sed_config (config_owner, config_cat, config_order, config_name, config_type, config_value) VALUES ('core', 'main', '07', 'mtmode', 3, '0');
 INSERT INTO sed_config (config_owner, config_cat, config_order, config_name, config_type, config_value) VALUES ('core', 'main', '07', 'mtres', 1, '');
 
