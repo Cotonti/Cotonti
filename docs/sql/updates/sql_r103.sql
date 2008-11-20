a070d9e881a14e8dd663af02a04b0a08ef9575ed
Ozan Ciga <ozan.ciga@gmail.com>
2008-11-20 20:35:04 +0000

    SQL fix.

--- docs/sql/updates/sql_r103.sql
+++ docs/sql/updates/sql_r103.sql
@@ -1,2 +1,2 @@
 /* r103 */
-DELETE FROM `sed_config` WHERE `config_owner` = 'core' AND `config_cat` = 'forums' AND `config_name` = 'antibumpforum' LIMIT 1;
+DELETE FROM `sed_config` WHERE `config_owner` = 'core' AND `config_cat` = 'forums' AND `config_name` = 'antibumpforums' LIMIT 1;
