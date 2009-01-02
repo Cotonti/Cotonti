a633bc371cbea3dee457ed93c30e344be3de246d
Vladimir Sibirov <trustmaster@kodigy.com>
2009-01-02 18:36:56 +0000

    Seditio compatibility plugin which adds missing bbcodes.

---
+++ plugins/seditio/seditio.php
@@ -0,0 +1,28 @@
+<?php
+/* ====================
+[BEGIN_SED]
+File=plugins/seditio/seditio.php
+Version=0.0.2
+Updated=2009-jan-02
+Type=Plugin
+Author=Neocrome
+Description=
+[END_SED]
+
+[BEGIN_SED_EXTPLUGIN]
+Code=seditio
+Part=main
+File=seditio
+Hooks=standalone
+Tags=
+Minlevel=0
+Order=10
+[END_SED_EXTPLUGIN]
+
+==================== */
+
+if (!defined('SED_CODE')) { die('Wrong URL.'); }
+
+// Dummy standalone plugin, fill it with code you are missing from Seditio
+
+?>
