70504eb30bc09bc3f36d893e5d0101d02b55926d
Pavel Tkachenko <dayver@dayver.net>
2009-01-03 18:55:30 +0000

    xhtml code removed from php for plug whosonline

---
+++ docs/sql/updates/sql_r242.sql
@@ -0,0 +1,5 @@
+/* xhtml code removed from php for plug whosonline*/
+
+INSERT INTO `sed_config` VALUES ('plug', 'whosonline', '1', 'showavatars', 3, '1', '', 'Display avatars of users?');
+INSERT INTO `sed_config` VALUES ('plug', 'whosonline', '2', 'miniavatar_x', 1, '16', '', 'The size of a mini-avatars on the axis x, in pixels');
+INSERT INTO `sed_config` VALUES ('plug', 'whosonline', '3', 'miniavatar_y', 1, '16', '', 'The size of a mini-avatars on the axis y, in pixels');
--- plugins/whosonline/lang/whosonline.en.lang.php
+++ plugins/whosonline/lang/whosonline.en.lang.php
@@ -1,24 +1,25 @@
 <?PHP
-
 /* ====================
-Seditio - Website engine
-Copyright Neocrome
-http://www.neocrome.net
 [BEGIN_SED]
 File=plugins/whosonline/lang/whosonline.en.lang.php
-Version=101
-Updated=2006-mar-15
-Type=
-Author=Neocrome
-Description=
+Version=0.0.2
+Updated=2009-jan-03
+Type=English language file
+Author=Neocrome & Cotonti Team
+Description=Cotonti - Website engine http://www.cotonti.com
 [END_SED]
 ==================== */
+
+$L['cfg_showavatars'] = array("Display avatars of users?");
+$L['cfg_miniavatar_x'] = array("The size of a mini-avatars on the axis x","in pixels");
+$L['cfg_miniavatar_y'] = array("The size of a mini-avatars on the axis y","in pixels");
 
 $L['plu_title'] = "Who's online ?";
 $L['plu_mostonline'] = "Most users ever online was ";
 $L['plu_therescurrently'] = "There's currently ";
 $L['plu_visitors'] = " visitor(s) and ";
 $L['plu_members'] = " registered member(s) online.";
+$L['plu_user_avatar'] = "Аватар";
 $L['plu_lastseen1'] = "Last seen";
 $L['plu_in'] = "in";
 $L['plu_visitor'] = "Visitor";
