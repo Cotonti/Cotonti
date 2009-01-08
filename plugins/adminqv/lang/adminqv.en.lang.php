47a9fa75a36ce063b71dae4f1d957626b9cf68e3
Michael Parfenov <mickael23@gmail.com>
2009-01-08 15:59:17 +0000

    Most used links for configuring site in admin area.

--- plugins/adminqv/lang/adminqv.en.lang.php
+++ plugins/adminqv/lang/adminqv.en.lang.php
@@ -17,6 +17,36 @@ Description=
 
 if (!defined('SED_CODE')) { die('Wrong URL.'); }
 
+$L['plu_ql_b1_title'] = "Site properties";
+$L['plu_ql_b1_1'] = "Basic config";
+$L['plu_ql_b1_2'] = "Titles";
+$L['plu_ql_b1_3'] = "Skin and localization";
+$L['plu_ql_b1_4'] = "Menu slots in header.tpl";
+$L['plu_ql_b1_5'] = "Language";
+$L['plu_ql_b1_6'] = "Time and date";
+$L['plu_ql_b1_7'] = "Trash";
+
+$L['plu_ql_b2_title'] = "Pages";
+$L['plu_ql_b2_1'] = "Structure of the pages (categories)";
+$L['plu_ql_b2_2'] = "Extra fields for pages";
+$L['plu_ql_b2_3'] = "Set default sorting order";
+$L['plu_ql_b2_4'] = "Parser";
+
+$L['plu_ql_b3_title'] = "Users";
+$L['plu_ql_b3_1'] = "Basic config";
+$L['plu_ql_b3_2'] = "Exra fields for users";
+$L['plu_ql_b3_3'] = "Personal file system";
+$L['plu_ql_b3_4'] = "Set permissions and PFS size";
+$L['plu_ql_b3_5'] = "Private messages";
+
+$L['plu_ql_b4_title'] = "Core functional";
+$L['plu_ql_b4_1'] = "Plugins";
+$L['plu_ql_b4_2'] = "Forum";
+$L['plu_ql_b4_3'] = "Page comments";
+$L['plu_ql_b4_4'] = "Polls";
+$L['plu_ql_b4_5'] = "Page ratings";
+
+
 $L['plu_title'] = "Administrator's QuickView";
 
 $L['plu_pastdays'] = "Activity for the past 7 days";
