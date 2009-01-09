5b66e74f56198570b96867a6468ebf1a5e3de566
Pavel Tkachenko <dayver@dayver.net>
2009-01-09 01:59:58 +0000

    Admin xhtml code introduced in the TPL file

--- system/core/admin/admin.cache.inc.php
+++ system/core/admin/admin.cache.inc.php
@@ -1,20 +1,16 @@
 <?PHP
-
 /* ====================
-Seditio - Website engine
-Copyright Neocrome
-http://www.neocrome.net
 [BEGIN_SED]
 File=admin.cache.inc.php
-Version=101
-Updated=2006-mar-15
+Version=0.0.2
+Updated=2009-jan-03
 Type=Core.admin
-Author=Neocrome
-Description=Administration panel
+Author=Neocrome & Cotonti Team
+Description=Internal cache (Cotonti - Website engine http://www.cotonti.com Copyright (c) Cotonti Team 2009 BSD License)
 [END_SED]
 ==================== */
 
-if ( !defined('SED_CODE') || !defined('SED_ADMIN') ) { die('Wrong URL.'); }
+if (!defined('SED_CODE') || !defined('SED_ADMIN')) { die('Wrong URL.'); }
 
 list($usr['auth_read'], $usr['auth_write'], $usr['isadmin']) = sed_auth('admin', 'a');
 sed_block($usr['isadmin']);
@@ -22,42 +18,50 @@ sed_block($usr['isadmin']);
 $adminpath[] = array(sed_url('admin', 'm=other'), $L['Other']);
 $adminpath[] = array (sed_url('admin', 'm=cache'), $L['adm_internalcache']);
 
+$t = new XTemplate(sed_skinfile('admin.cache.inc'));
+
 if ($a=='purge')
-	{
-	sed_check_xg();
-	sed_cache_clearall();
-	}
+{
+	$admincache = (sed_check_xg() && sed_cache_clearall()) ? $L['adm_purgeall_done'] : $L['Error'];
+}
 elseif ($a=='delete')
-	{
+{
 	sed_check_xg();
 	$sql = sed_sql_query("DELETE FROM $db_cache WHERE c_name='$id'");
-	}
+	$admincache = ($sql) ? $L['adm_delcacheitem'] : $L['Error'];
+}
 
 $sql = sed_sql_query("SELECT * FROM $db_cache WHERE 1 ORDER by c_name ASC");
-
-$adminmain .= "<p><a href=\"".sed_url('admin', 'm=cache')."\">".$L['Refresh']."</a> | ";
-$adminmain .= "<a href=\"".sed_url('admin', 'm=cache&a=purge&'.sed_xg())."\">".$L['adm_purgeall']."</a> | ";
-$adminmain .= "<a href=\"".sed_url('admin', 'm=cache&a=showall')."\">".$L['adm_showall']."</a></p>";
-$adminmain .= "<table class=\"cells\">";
-$adminmain .= "<tr><td class=\"coltop\">".$L['Delete']."</td><td class=\"coltop\">".$L['Item']."</td><td class=\"coltop\">".$L['Expire']."</td>";
-$adminmain .= "<td class=\"coltop\">".$L['Size']."</td><td class=\"coltop\">".$L['Value']."</td></tr>";
 $cachesize = 0;
 
 while ($row = sed_sql_fetcharray($sql))
-	{
+{
 	$row['c_value'] = sed_cc($row['c_value']);
 	$row['size'] = mb_strlen($row['c_value']);
 	$cachesize += $row['size'];
-	$adminmain .= "<tr><td style=\"text-align:center;\">[<a href=\"".sed_url('admin', 'm=cache&a=delete&id='.$row['c_name'].'&'.sed_xg())."\">x</a>]</td>";
-	$adminmain .= "<td>".$row['c_name']."</td>";
-	$adminmain .= "<td style=\"text-align:right;\">".($row['c_expire']-$sys['now'])."</td>";
-	$adminmain .= "<td style=\"text-align:right;\">".$row['size']."</td>";
+	$t -> assign(array(
+		"ADMIN_CACHE_ITEM_DEL_URL" => sed_url('admin', 'm=cache&a=delete&id='.$row['c_name'].'&'.sed_xg()),
+		"ADMIN_CACHE_ITEM_NAME" => $row['c_name'],
+		"ADMIN_CACHE_EXPIRE" => ($row['c_expire']-$sys['now']),
+		"ADMIN_CACHE_SIZE" => $row['size'],
+		"ADMIN_CACHE_VALUE" => ($a=='showall') ? $row['c_value'] : sed_cutstring($row['c_value'], 80)
+		));
+	$t -> parse("CACHE.ADMIN_CACHE_ROW");
+}
 
-	if ($a=='showall')
-		{ $adminmain .= "<td>".$row['c_value']."</td></tr>"; }
-   else
-		{ $adminmain .= "<td>".sed_cutstring($row['c_value'], 80)."</td></tr>"; }
-	}
-$adminmain .= "<tr><td colspan=\"3\">&nbsp;</td><td style=\"text-align:right;\">".$cachesize."</td><td>&nbsp;</td></tr></table>";
+if(!empty($admincache))
+{
+	$t -> assign(array("ADMIN_CACHE_MESAGE" => $admincache));
+	$t -> parse("CACHE.MESAGE");
+}
 
+$t -> assign(array(
+	"ADMIN_CACHE_URL_REFRESH" => sed_url('admin', 'm=cache'),
+	"ADMIN_CACHE_URL_PURGE" => sed_url('admin', 'm=cache&a=purge&'.sed_xg()),
+	"ADMIN_CACHE_URL_SHOWALL" => sed_url('admin', 'm=cache&a=showall'),
+	"ADMIN_CACHE_CACHESIZE" => $cachesize
+	));
+
+$t -> parse("CACHE");
+$adminmain = $t -> text("CACHE");
 ?>
