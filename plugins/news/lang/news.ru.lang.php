69cb01d8d1a94b9d830d54120254908ac24791f6
Pavel Mikulik <esclkm@gmail.com>
2009-01-16 10:43:22 +0000

    Translated into russian plugin settings

---
+++ plugins/news/lang/news.ru.lang.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Russian language file for cleaner
+ *
+ * @package Seditio-N
+ * @version 0.0.2
+ * @author eclkm
+ * @copyright (c) 2008 Cotonti Team
+ * @license BSD License
+ */
+$L['cfg_category']= array('Код родительской категории');
+$L['cfg_maxpages']= array('Отображать страниц');
+
+
+?>
