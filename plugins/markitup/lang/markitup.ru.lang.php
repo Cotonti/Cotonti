47982c6a6ea34b8ff917a75ba01e273a3977e4c1
Pavel Mikulik <esclkm@gmail.com>
2009-01-16 10:43:22 +0000

    Translated into russian plugin settings

---
+++ plugins/markitup/lang/markitup.ru.lang.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Russian language file for indexpolls
+ *
+ * @package Seditio-N
+ * @version 0.0.2
+ * @author esclkm
+ * @copyright (c) 2008 Cotonti Team
+ * @license BSD License
+ */
+$L['cfg_autorefresh']= array('Включить автообновление предпросмотра');
+$L['cfg_chili']= array('Включить тэги Chili');
+
+?>
---
+++ plugins/pagetextbyidn/lang/pagetextbyidn.ru.lang.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Russian language file for cleaner
+ *
+ * @package Seditio-N
+ * @version 0.0.2
+ * @author eclkm
+ * @copyright (c) 2008 Cotonti Team
+ * @license BSD License
+ */
+$L['cfg_link_to_edit']= array('Ссылка для редактирования текста', '{HREF_EDIT} - href конструкция для данной ссылки');
+$L['cfg_where']= array('Расположение ссылки редактирования текста','"before" - перед текстом, "after" - после текста,"do not include" - отключить ссылку редактирования теста');
+
+
+?>
