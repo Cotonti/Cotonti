36f2090b8b328c101cf8ba96da88c86c2ebfde05
Vladimir Sibirov <trustmaster@kodigy.com>
2009-02-14 12:16:22 +0000

    Fixes and optimizations in "who's online" part

--- system/common.php
+++ system/common.php
@@ -7,8 +7,8 @@ http://www.neocrome.net
 ==================== */
 
 /**
- * @package Seditio-N
- * @version 0.0.2
+ * @package Cotonti
+ * @version 0.0.3
  * @author Neocrome, Cotonti Team
  * @copyright Copyright (c) Cotonti Team 2008
  * @license BSD
@@ -22,7 +22,6 @@ if (!defined('SED_CODE')) { die('Wrong URL.'); }
 	or die('Cotonti system requirements: PHP 5.1 or above.');
 extension_loaded('mbstring')
 	or die('Cotonti system requirements: mbstring PHP extension must be loaded.');
-
 
 /* ======== First... ======== */
 
@@ -206,9 +205,9 @@ if(!empty($_COOKIE['COTONTI']) || !empty($_SESSION['COTONTI']))
 					|| ($sys['now_offset'] - $_SESSION['saltstamp'] < 30
 						&& $u_passhash == $_SESSION['oldhash']))
 				&& $row['user_maingrp'] > 3
-				&& (!$cfg['ipcheck'] || $row['user_lastip'] == $usr['ip']))
+				&& ($cfg['ipcheck']==FALSE || $row['user_lastip'] == $usr['ip']))
 			{
-				$usr['id'] = $row['user_id'];
+				$usr['id'] = (int) $row['user_id'];
 				$usr['sessionid'] = ($cfg['authmode']==1) ? md5($row['user_lastvisit']) : session_id();
 				$usr['name'] = $row['user_name'];
 				$usr['maingrp'] = $row['user_maingrp'];
@@ -331,11 +330,54 @@ $b = sed_import('b','G','ALP',24);
 
 if (!$cfg['disablewhosonline'] || $cfg['shieldenabled'])
 {
+	if ($usr['id']>0)
+	{
+		$sql = sed_sql_query("SELECT * FROM $db_online WHERE online_userid=".$usr['id']);
 
-	$sql = sed_sql_query("DELETE FROM $db_online WHERE online_lastseen<'$online_timedout'");
+		if ($row = sed_sql_fetcharray($sql))
+		{
+			$online_count = 1;
+			if ($cfg['shieldenabled'])
+			{
+				$shield_limit = $row['online_shield'];
+				$shield_action = $row['online_action'];
+				$shield_hammer = sed_shield_hammer($row['online_hammer'],$shield_action,$row['online_lastseen']);
+			}
+			sed_sql_query("UPDATE $db_online SET online_lastseen='".$sys['now']."', online_location='".sed_sql_prep($location)."', online_subloc='".sed_sql_prep($sys['sublocation'])."', online_hammer=".(int)$shield_hammer." WHERE online_userid=".$usr['id']);
+		}
+		else
+		{
+			sed_sql_query("INSERT INTO $db_online (online_ip, online_name, online_lastseen, online_location, online_subloc, online_userid, online_shield, online_hammer) VALUES ('".$usr['ip']."', '".sed_sql_prep($usr['name'])."', ".(int)$sys['now'].", '".sed_sql_prep($location)."',  '".sed_sql_prep($sys['sublocation'])."', ".(int)$usr['id'].", 0, 0)");
+		}
+	}
+	else
+	{
+		$sql = sed_sql_query("SELECT * FROM $db_online WHERE online_ip='".$usr['ip']."'");
+		$online_count = sed_sql_numrows($sql);
+
+		if ($online_count>0)
+		{
+			if ($cfg['shieldenabled'])
+			{
+				if ($row = sed_sql_fetcharray($sql))
+				{
+					$shield_limit = $row['online_shield'];
+					$shield_action = $row['online_action'];
+					$shield_hammer = sed_shield_hammer($row['online_hammer'],$shield_action,$row['online_lastseen']);
+				}
+			}
+			sed_sql_query("UPDATE $db_online SET online_lastseen='".$sys['now']."', online_location='".$location."', online_subloc='".sed_sql_prep($sys['sublocation'])."', online_hammer=".(int)$shield_hammer." WHERE online_ip='".$usr['ip']."'");
+		}
+		else
+		{
+			sed_sql_query("INSERT INTO $db_online (online_ip, online_name, online_lastseen, online_location, online_subloc, online_userid, online_shield, online_hammer) VALUES ('".$usr['ip']."', 'v', ".(int)$sys['now'].", '".$location."', '".sed_sql_prep($sys['sublocation'])."', -1, 0, 0)");
+		}
+	}
+
+	$sql = sed_sql_query("DELETE FROM $db_online WHERE online_lastseen<$online_timedout");
 	$sql = sed_sql_query("SELECT COUNT(*) FROM $db_online WHERE online_name='v'");
 	$sys['whosonline_vis_count'] = sed_sql_result($sql, 0, 'COUNT(*)');
-	$sql = sed_sql_query("SELECT o.online_name, o.online_userid FROM $db_online o WHERE o.online_name NOT LIKE 'v' ORDER BY online_name ASC");
+	$sql = sed_sql_query("SELECT DISTINCT o.online_name, o.online_userid FROM $db_online o WHERE o.online_name != 'v' ORDER BY online_name ASC");
 	$sys['whosonline_reg_count'] = sed_sql_numrows($sql);
 	$sys['whosonline_all_count'] = $sys['whosonline_reg_count'] + $sys['whosonline_vis_count'];
 
--- system/footer.php
+++ system/footer.php
@@ -4,15 +4,15 @@
 Seditio - Website engine
 Copyright Neocrome
 http://www.neocrome.net
-[BEGIN_SED]
-File=system/footer.php
-Version=122
-Updated=2008-feb-18
-Type=Core
-Author=Neocrome
-Description=Global footer
-[END_SED]
 ==================== */
+
+/**
+ * @package Cotonti
+ * @version 0.0.3
+ * @author Neocrome, Cotonti Team
+ * @copyright Copyright (c) Cotonti Team 2008
+ * @license BSD
+ */
 
 if (!defined('SED_CODE')) { die('Wrong URL.'); }
 
@@ -26,92 +26,27 @@ $i = explode(' ', microtime());
 $sys['endtime'] = $i[1] + $i[0];
 $sys['creationtime'] = round(($sys['endtime'] - $sys['starttime']), 3);
 
-/* ========================================================
-#
-#  If you're not owner of a commercial licence or a copyright removal,
-#  please do not delete the copyright line in the footer, thanks.
-#  With doing this you'd break the licence agrement for personal users,
-#  you probably won't go in jail for this but that's bad.
-#  You can order a copyright line free version here :
-#  http://www.neocrome.net/
-#
-========================================================== */
-
 $out['creationtime'] = (!$cfg['disablesysinfos']) ? $L['foo_created'].' '.$sys['creationtime'].' '.$L['foo_seconds'] : '';
 $out['sqlstatistics'] = ($cfg['showsqlstats']) ? $L['foo_sqltotal'].': '.round($sys['tcount'], 3).' '.$L['foo_seconds'].' - '.$L['foo_sqlqueries'].': '.$sys['qcount']. ' - '.$L['foo_sqlaverage'].': '.round(($sys['tcount']/$sys['qcount']), 5).' '.$L['foo_seconds'] : '';
 $out['bottomline'] = $cfg['bottomline'];
 $out['bottomline'] .= ($cfg['keepcrbottom']) ? '<br />'.$out['copyright'] : '';
 
-/* ======== Who's online (part 2) ======== */
-
-if (!$cfg['disablewhosonline'] || $cfg['shieldenabled'])
-	{
-	if ($usr['id']>0)
-		{
-		$sql = sed_sql_query("SELECT online_id FROM $db_online WHERE online_userid='".$usr['id']."'");
-
-		if ($row = sed_sql_fetcharray($sql))
-			{
-			$online_count = 1;
-			if ($cfg['shieldenabled'])
-				{
-				$sql2 = sed_sql_query("SELECT online_shield, online_action, online_hammer, online_lastseen FROM $db_online WHERE online_userid='".$usr['id']."'");
-				if ($row = sed_sql_fetcharray($sql2))
-					{
-					$shield_limit = $row['online_shield'];
-					$shield_action = $row['online_action'];
-					$shield_hammer = sed_shield_hammer($row['online_hammer'],$shield_action,$row['online_lastseen']);
-					}
-				}
-			$sql2 = sed_sql_query("UPDATE $db_online SET online_lastseen='".$sys['now']."', online_location='".sed_sql_prep($location)."', online_subloc='".sed_sql_prep($sys['sublocation'])."', online_hammer=".(int)$shield_hammer." WHERE online_userid='".$usr['id']."'");
-			}
-		else
-         {
-         $sql2 = sed_sql_query("INSERT INTO $db_online (online_ip, online_name, online_lastseen, online_location, online_subloc, online_userid, online_shield, online_hammer) VALUES ('".$usr['ip']."', '".sed_sql_prep($usr['name'])."', ".(int)$sys['now'].", '".sed_sql_prep($location)."',  '".sed_sql_prep($sys['sublocation'])."', ".(int)$usr['id'].", 0, 0)");
-         }
-      }
-   else
-      {
-      $sql = sed_sql_query("SELECT COUNT(*) FROM $db_online WHERE online_ip='".$usr['ip']."'");
-      $online_count = sed_sql_result($sql,0,'COUNT(*)');
-
-      if ($online_count>0)
-         {
-         if ($cfg['shieldenabled'])
-            {
-            $sql2 = sed_sql_query("SELECT online_shield, online_action, online_hammer, online_lastseen FROM $db_online WHERE online_ip='".$usr['ip']."'");
-            if ($row = sed_sql_fetcharray($sql2))
-               {
-               $shield_limit = $row['online_shield'];
-               $shield_action = $row['online_action'];
-               $shield_hammer = sed_shield_hammer($row['online_hammer'],$shield_action,$row['online_lastseen']);
-               }
-            }
-         $sql2 = sed_sql_query("UPDATE $db_online SET online_lastseen='".$sys['now']."', online_location='".$location."', online_subloc='".sed_sql_prep($sys['sublocation'])."', online_hammer=".(int)$shield_hammer." WHERE online_ip='".$usr['ip']."'");
-         }
-      else
-         {
-         $sql2 = sed_sql_query("INSERT INTO $db_online (online_ip, online_name, online_lastseen, online_location, online_subloc, online_userid, online_shield, online_hammer) VALUES ('".$usr['ip']."', 'v', ".(int)$sys['now'].", '".$location."', '".sed_sql_prep($sys['sublocation'])."', -1, 0, 0)");
-         }
-      }
-	}
-
 if ($cfg['devmode'])
-	{
+{
 	$out['devmode'] = "<h4>Dev-mode :</h4><table><tr><td><em>SQL query</em></td><td><em>Duration</em></td><td><em>Timeline</em></td><td><em>Query</em></td></tr>";
 	$out['devmode'] .= "<tr><td colspan=\"2\">BEGIN</td>";
 	$out['devmode'] .= "<td style=\"text-align:right;\">0.000 ms</td><td>&nbsp;</td></tr>";
 	foreach ($sys['devmode']['queries'] as $k => $i)
-		{
+	{
 		$out['devmode'] .= "<tr><td>#".$i[0]." &nbsp;</td>";
 		$out['devmode'] .= "<td style=\"text-align:right;\">".sprintf("%.3f",round($i[1]*1000, 3))." ms</td>";
 		$out['devmode'] .= "<td style=\"text-align:right;\">".sprintf("%.3f",round($sys['devmode']['timeline'][$k]*1000, 3))." ms</td>";
 		$out['devmode'] .= "<td style=\"text-align:left;\">".sed_cc($i[2])."</td></tr>";
-		}
+	}
 	$out['devmode'] .= "<tr><td colspan=\"2\">END</td>";
 	$out['devmode'] .= "<td style=\"text-align:right;\">".sprintf("%.3f",$sys['creationtime'])." ms</td><td>&nbsp;</td></tr>";
 	$out['devmode'] .= "</table><br />Total:".round($sys['tcount'],4)."s - Queries:".$sys['qcount']. " - Average:".round(($sys['tcount']/$sys['qcount']),5)."s/q";
-	}
+}
 
 /*
 ========= DEBUG:START =========
@@ -132,9 +67,9 @@ if (is_array($extp))
 /* ===== */
 
 if ($cfg['enablecustomhf'])
-	{ $mskin = sed_skinfile(array('footer', mb_strtolower($location))); }
+{ $mskin = sed_skinfile(array('footer', mb_strtolower($location))); }
 else
-	{ $mskin = "skins/".$usr['skin']."/footer.tpl"; }
+{ $mskin = "skins/".$usr['skin']."/footer.tpl"; }
 $t = new XTemplate($mskin);
 
 $t->assign(array (
@@ -151,13 +86,13 @@ $t->assign(array (
 /* === Hook === */
 $extp = sed_getextplugins('footer.tags');
 if (is_array($extp))
-	{ foreach($extp as $k => $pl) { include_once($cfg['plugins_dir'].'/'.$pl['pl_code'].'/'.$pl['pl_file'].'.php'); } }
+{ foreach($extp as $k => $pl) { include_once($cfg['plugins_dir'].'/'.$pl['pl_code'].'/'.$pl['pl_file'].'.php'); } }
 /* ===== */
 
 if ($usr['id']>0)
-	{ $t->parse("FOOTER.USER"); }
+{ $t->parse("FOOTER.USER"); }
 else
-	{ $t->parse("FOOTER.GUEST"); }
+{ $t->parse("FOOTER.GUEST"); }
 
 $t->parse("FOOTER");
 $t->out("FOOTER");
