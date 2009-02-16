0003dcfa28f803f21efec2831ac186eb573a5734
Vladimir Sibirov <trustmaster@kodigy.com>
2009-02-16 10:12:37 +0000

    Added a missing file which could not commit with r569 for unknown reason

---
+++ plugins/tags/inc/config.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Array of styles and levels in tag cloud
+ * max. entries => CSS class name
+ */
+
+$tc_styles = array(
+	1 => 'xs',
+	5 => 's',
+	20 => 'm',
+	50 => 'l',
+	999999999 => 'xl'
+)
+?>
