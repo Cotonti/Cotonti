658cab096e561e487853af52efe392fc3cae6805
Pavel Tkachenko <dayver2009@gmail.com>
2009-03-01 17:09:35 +0000

    fix for last changeset r621

--- system/core/admin/admin.polls.inc.php
+++ system/core/admin/admin.polls.inc.php
@@ -33,8 +33,13 @@ $variants['forum']= array($L['Forums'], "forum");
 
 /* === Hook === */
 $extp = sed_getextplugins('adim.polls.first');
-if (is_array($extp))
-{ foreach($extp as $k => $pl) { include_once($cfg['plugins_dir'].'/'.$pl['pl_code'].'/'.$pl['pl_file'].'.php'); } }
+if(is_array($extp))
+{
+	foreach($extp as $k => $pl)
+	{
+		include_once($cfg['plugins_dir'].'/'.$pl['pl_code'].'/'.$pl['pl_file'].'.php');
+	}
+}
 /* ===== */
 
 if($a == 'delete')
@@ -89,25 +94,22 @@ if(!empty($adminpollsmsg))
 if(!$filter)
 {
     $poll_type = "1";
-    $poll_filter= "";
+    $poll_filter = "";
 }
 else
 {
     $poll_type = "poll_type='$filter'";
-    $poll_filter ="&filter=$filter";
+    $poll_filter = "&filter=$filter";
 }
 
 $sql = sed_sql_query("SELECT COUNT(*) FROM $db_polls WHERE $poll_type");
 $totalitems = sed_sql_result($sql,0,"COUNT(*)");
 
-
 $sql = sed_sql_query("SELECT * FROM $db_polls
 					WHERE $poll_type ORDER BY poll_id DESC LIMIT $d, ".$cfg['maxrowsperpage']);
 
-
 $pagnav = sed_pagination(sed_url('admin','m=polls'.$poll_filter), $d, $totalitems, $cfg['maxrowsperpage']);
 list($pagination_prev, $pagination_next) = sed_pagination_pn(sed_url('admin', 'm=polls'.$poll_filter), $d, $totalitems, $cfg['maxrowsperpage'], TRUE);
-
 
 $ii = 0;
 $indexheader =false;
@@ -119,7 +121,6 @@ $extp = sed_getextplugins('admin.polls.loop');
 
 while($row = sed_sql_fetcharray($sql))
 {
-
 	$id = $row['poll_id'];
 	$type = $row['poll_type'];
 
@@ -135,20 +136,25 @@ while($row = sed_sql_fetcharray($sql))
 	$t -> assign(array(
 		"ADMIN_POLLS_ROW_POLL_CREATIONDATE" => date($cfg['formatyearmonthday'], $row['poll_creationdate']),
 		"ADMIN_POLLS_ROW_POLL_TYPE" => sed_cc($type),
-		"ADMIN_POLLS_ROW_POLL_URL" => sed_url('admin', "m=polls&n=options&id=".$row['poll_id']),
+		"ADMIN_POLLS_ROW_POLL_URL" => sed_url('admin', "m=polls".$poll_filter."&n=options&d=".$d."&id=".$row['poll_id']),
 		"ADMIN_POLLS_ROW_POLL_TEXT" => sed_cc($row['poll_text']),
 		"ADMIN_POLLS_ROW_POLL_TOTALVOTES" => $totalvotes,
 		"ADMIN_POLLS_ROW_POLL_URL_DEL" => sed_url('admin', "m=polls".$poll_filter."&a=delete&id=".$id."&".sed_xg()),
-		"ADMIN_POLLS_ROW_POLL_URL_RES" => sed_url('admin', "m=polls".$poll_filter."&a=reset&id=".$id."&".sed_xg()),
+		"ADMIN_POLLS_ROW_POLL_URL_RES" => sed_url('admin', "m=polls".$poll_filter."&a=reset&d=".$d."&id=".$id."&".sed_xg()),
 		"ADMIN_POLLS_ROW_POLL_URL_BMP" => sed_url('admin', "m=polls".$poll_filter."&a=bump&id=".$id."&".sed_xg()),
 		"ADMIN_POLLS_ROW_POLL_URL_OPN" => $admtypepoll
 	));
 
 	/* === Hook - Part2 : Include === */
-	if (is_array($extp))
-	{ foreach($extp as $k => $pl) { include($cfg['plugins_dir'].'/'.$pl['pl_code'].'/'.$pl['pl_file'].'.php'); } }
+	if(is_array($extp))
+	{
+		foreach($extp as $k => $pl)
+		{
+			 include($cfg['plugins_dir'].'/'.$pl['pl_code'].'/'.$pl['pl_file'].'.php');
+		}
+	}
 	/* ===== */
-    
+
 	$t -> parse("POLLS.POLLS_ROW");
 
 	$ii++;
@@ -157,15 +163,15 @@ while($row = sed_sql_fetcharray($sql))
 if($n == 'options')
 {
 	$poll_id = sed_import('id','G','TXT');
-	$adminpath[] = array (sed_url('admin', 'm=polls&n=options&id=$poll_id'), $L['Options']." (#$id)");
-	$adminmain .= $L['editdeleteentries'];
+	$adminpath[] = array (sed_url('admin', 'm=polls'.$poll_filter.'&n=options&id='.$poll_id.'&d='.$d), $L['Options']." (#$id)");
+	$formname = $L['editdeleteentries'];
 	$send_button = $L['Update'];
 }
 elseif(!empty($error_string))
 {
 	if($poll_id != 'new')
 	{
-		$adminpath[] = array (sed_url('admin', 'm=polls&n=options&id=$poll_id'), $L['Options']." (#$id)");
+		$adminpath[] = array (sed_url('admin', 'm=polls'.$poll_filter.'&n=options&id='.$poll_id.'&d='.$d), $L['Options']." (#$id)");
 		$formname = $L['editdeleteentries'];
 		$send_button = $L['Update'];
 	}
@@ -182,30 +188,33 @@ else
 	$send_button = $L['Create'];
 }
 
-$pollfilter="<form id=\"jump\"><select name=\"jumpbox\" size=\"1\" onchange=\"redirect(this)\">";
 foreach($variants as $val)
 {
-	 $checked = ($filter==$val[1]) ? "selected='selected'" : "";
-	if ($val[1])
+	$checked = ($filter == $val[1]) ? " selected='selected'" : "";
+	if($val[1])
 	{
-	 $val[1]="&filter=".$val[1];
+		$val[1]="&filter=".$val[1];
 	}
-$pollfilter.="<option value=\"".sed_url('admin', "m=polls".$val[1])." \" $checked>".$val[0]."</option>";
-}
-$pollfilter.="</select></form>";
 
-list($poll_text, $poll_options, $poll_date, $poll_settings)=sed_poll_edit_form($poll_id, 1);
+	$t -> assign(array(
+		"ADMIN_POLLS_ROW_FILTER_VALUE" => sed_url('admin', "m=polls".$val[1]),
+		"ADMIN_POLLS_ROW_FILTER_CHECKED" => $checked,
+		"ADMIN_POLLS_ROW_FILTER_NAME" => $val[0]
+	));
+	$t -> parse("POLLS.POLLS_ROW_FILTER");
+}
+
+list($poll_text, $poll_options, $poll_date, $poll_settings) = sed_poll_edit_form($poll_id, 1);
 
 $t -> assign(array(
 	"ADMIN_POLLS_CONF_URL" => sed_url('admin', "m=config&n=edit&o=core&p=polls"),
 	"ADMIN_POLLS_PAGINATION_PREV" => $pagination_prev,
-	"ADMIN_POLLS_FILTER" => $pollfilter,
 	"ADMIN_POLLS_PAGNAV" => $pagnav,
 	"ADMIN_POLLS_PAGINATION_NEXT" => $pagination_next,
 	"ADMIN_POLLS_TOTALITEMS" => $totalitems,
 	"ADMIN_POLLS_ON_PAGE" => $ii,
 	"ADMIN_POLLS_FORMNAME" => $formname,
-	"ADMIN_POLLS_FORM_URL" => sed_url('admin', "m=polls"),
+	"ADMIN_POLLS_FORM_URL" => ($poll_id != 'new') ? sed_url('admin', "m=polls".$poll_filter."&d=".$d) : sed_url('admin', "m=polls"),
 	"ADMIN_POLLS_POLL_TEXT" => $poll_text,
 	"ADMIN_POLLS_POLL_DATE" => $poll_date,
 	"ADMIN_POLLS_POLL_OPTIONS" => $poll_options,
@@ -213,11 +222,16 @@ $t -> assign(array(
 	"ADMIN_POLLS_SEND_BUTTON" => $send_button
 ));
 
-	/* === Hook  === */
-	$extp = sed_getextplugins('admin.polls.tags');
-	if (is_array($extp))
-	{ foreach($extp as $k => $pl) { include_once($cfg['plugins_dir'].'/'.$pl['pl_code'].'/'.$pl['pl_file'].'.php'); } }
-	/* ===== */
+/* === Hook  === */
+$extp = sed_getextplugins('admin.polls.tags');
+if(is_array($extp))
+{
+	foreach($extp as $k => $pl)
+	{
+		include_once($cfg['plugins_dir'].'/'.$pl['pl_code'].'/'.$pl['pl_file'].'.php');
+	}
+}
+/* ===== */
 
 $t -> parse("POLLS");
 $adminmain = $t -> text("POLLS");
