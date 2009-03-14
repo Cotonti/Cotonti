319a922fba7d295fdc8ef2f90c99f1af87565394
Dmitri Beliavski <mail@seditio.by>
2009-03-14 10:55:12 +0000

    1. Applied uniform lang-file template 2. Removed redundant lang-strings

--- plugins/adminqv/lang/adminqv.en.lang.php
+++ plugins/adminqv/lang/adminqv.en.lang.php
@@ -1,48 +1,25 @@
 <?PHP
 /**
- * English language file
+ * English Language File for AdminQV Plugin
  *
  * @package Cotonti
- * @version 0.0.3
- * @author Neocrome, Cotonti Team
+ * @version 0.1.0
+ * @author Cotonti Translators Team
  * @copyright Copyright (c) Cotonti Team 2008-2009
  * @license BSD
  */
 
-$L['plu_ql_header'] = "Administration panel";
-
-$L['plu_ql_b1_title'] = "Site properties";
-$L['plu_ql_b1_1'] = "Basic config";
-$L['plu_ql_b1_2'] = "Titles";
-$L['plu_ql_b1_3'] = "Skin and localization";
-$L['plu_ql_b1_4'] = "Menu slots in header.tpl";
-$L['plu_ql_b1_5'] = "Language";
-$L['plu_ql_b1_6'] = "Time and date";
-$L['plu_ql_b1_7'] = "Trash";
-
-$L['plu_ql_b2_title'] = "Pages";
-$L['plu_ql_b2_1'] = "Structure of the pages (categories)";
-$L['plu_ql_b2_2'] = "Extra fields for pages";
-$L['plu_ql_b2_3'] = "Set default sorting order";
-$L['plu_ql_b2_4'] = "Parser";
-
-$L['plu_ql_b3_title'] = "Users";
-$L['plu_ql_b3_1'] = "Basic config";
-$L['plu_ql_b3_2'] = "Extra fields for users";
-$L['plu_ql_b3_3'] = "Personal file system";
-$L['plu_ql_b3_4'] = "Set permissions and PFS size";
-$L['plu_ql_b3_5'] = "Private messages";
-
-$L['plu_ql_b4_title'] = "Core functional";
-$L['plu_ql_b4_1'] = "Plugins";
-$L['plu_ql_b4_2'] = "Forum";
-$L['plu_ql_b4_3'] = "Page comments";
-$L['plu_ql_b4_4'] = "Polls";
-$L['plu_ql_b4_5'] = "Page ratings";
-
+/**
+ * Plugin Title & Subtitle
+ */
 
 $L['plu_title'] = "Administrator's QuickView";
 
+/**
+ * Plugin Body
+ */
+
+$L['plu_hitsmonth'] = "Hits for the past 15 days";
 $L['plu_pastdays'] = "Activity for the past 7 days";
 
 $L['plu_newusers'] = "New members";
@@ -57,6 +34,23 @@ $L['plu_db_indexsize'] = "SQL database, index size (KB)";
 $L['plu_db_datassize'] = "SQL database, datas size (KB)";
 $L['plu_db_totalsize'] = "SQL database, total size (KB)";
 
-$L['plu_hitsmonth'] = "Hits for the past 15 days";
+$L['plu_ql_b1_title'] = "Site properties";
+$L['plu_ql_b1_1'] = "Basic config";
+$L['plu_ql_b1_2'] = "Titles";
+$L['plu_ql_b1_3'] = "Skin and charset";
+$L['plu_ql_b1_4'] = "Menu slots in tpl-files";
+$L['plu_ql_b1_5'] = "Language";
+$L['plu_ql_b1_6'] = "Time and date";
+
+$L['plu_ql_b2_1'] = "Structure of the pages (categories)";
+$L['plu_ql_b2_2'] = "Extra fields for pages";
+$L['plu_ql_b2_3'] = "Set default sorting order";
+$L['plu_ql_b2_4'] = "Parser config";
+
+$L['plu_ql_b3_1'] = "Basic config";
+$L['plu_ql_b3_2'] = "Extra fields for users";
+$L['plu_ql_b3_4'] = "PFS permissions";
+
+$L['plu_ql_b4_title'] = "Core functional";
 
 ?>
