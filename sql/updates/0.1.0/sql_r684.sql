93f253711a446e81f087b95aa44f3dfd2e827f8e
Ozan Ciga <ozan.ciga@gmail.com>
2009-04-03 20:28:29 +0000

    Misspelling.

--- sql/updates/0.1.0/sql_r684.sql
+++ sql/updates/0.1.0/sql_r684.sql
@@ -1,6 +1,6 @@
 /* r684 Increase lengths */
 ALTER TABLE sed_forum_topics CHANGE ft_title ft_title VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '';
-ALTER TABLE sed_forum_topics CHANGE ft_title ft_desc VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '';
+ALTER TABLE sed_forum_topics CHANGE ft_desc ft_desc VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '';
 
 ALTER TABLE sed_forum_sections CHANGE fs_lt_title fs_lt_title VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '';
 
