348243dbb6461664a217213ccf2eab03ec390853
Vladimir Sibirov <trustmaster@kodigy.com>
2009-06-25 18:22:14 +0000

    Fixed #305

--- system/functions.php
+++ system/functions.php
@@ -1748,12 +1748,25 @@ function sed_cache_clear($name)
  */
 function sed_cache_clearall()
 {
-	global $db_cache,$db_pages,$db_forum_posts,$db_pm;
+	global $db_cache;
 	sed_sql_query("DELETE FROM $db_cache");
-	sed_sql_query("UPDATE $db_pages SET page_html=''");
-	sed_sql_query("UPDATE $db_forum_posts SET fp_html=''");
-	sed_sql_query("UPDATE $db_pm SET pm_html = ''");
-	return(TRUE);
+	return TRUE;
+}
+
+/**
+ * Clears HTML-cache
+ *
+ * @todo Add trigger support here to clean non-standard html fields
+ * @return bool
+ */
+function sed_cache_clearhtml()
+{
+	global $db_pages, $db_forum_posts, $db_pm;
+	$res = TRUE;
+	$res &= sed_sql_query("UPDATE $db_pages SET page_html=''");
+	$res &= sed_sql_query("UPDATE $db_forum_posts SET fp_html=''");
+	$res &= sed_sql_query("UPDATE $db_pm SET pm_html = ''");
+	return $res;
 }
 
 /**
