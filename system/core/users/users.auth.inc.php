fa5ceb1c14aeb48bc49fcefdbcdfe51a888f746f
dervan <dervvan@mail.ru>
2009-08-06 21:52:10 +0000

    xp/xg corrected. news plugin: added [more] tag for HTML parsing.

--- system/core/users/users.auth.inc.php
+++ system/core/users/users.auth.inc.php
@@ -100,7 +100,7 @@ if ($a=='check')
 
 		$sql = sed_sql_query("DELETE FROM $db_online WHERE online_userid='-1' AND online_ip='".$usr['ip']."' LIMIT 1");
 		sed_uriredir_apply($cfg['redirbkonlogin']);
-		sed_redirect(empty($redirect) ? sed_url('index') : base64_decode($redirect));
+		sed_uriredir_redirect(empty($redirect) ? sed_url('index') : base64_decode($redirect));
 		exit;
 	}
 	else
