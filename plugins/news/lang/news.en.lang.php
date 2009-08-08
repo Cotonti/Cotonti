ac67828fde0b2001c886f1351bd2d10378464b34
Pavel Mikulik <esclkm@gmail.com>
2009-08-08 16:25:13 +0000

    I make an mistake with news lang files (lang files file from 070 was updated in genua branch). in this revision fix this problem

--- plugins/news/lang/news.en.lang.php
+++ plugins/news/lang/news.en.lang.php
@@ -1,9 +1,9 @@
-<?PHP
+<?php
 /**
  * English Language File for News Plugin
  *
  * @package Cotonti
- * @version 0.1.0
+ * @version 0.7.0
  * @author Cotonti Translators Team
  * @copyright Copyright (c) Cotonti Team 2008-2009
  * @license BSD
@@ -15,10 +15,18 @@ defined('SED_CODE') or die('Wrong URL.');
  * Plugin Config
  */
 
-$L['cfg_category'] = array('News category codes, comma separated',
-'Main (first) news category is displayed on main page using {INDEX_NEWS} tag in index.tpl.<br />
-Other news categories are displayed on main page using <strong>{INDEX_NEWS_CATEGORYCODE}</strong> tag in index.tpl. Use <strong>news.categorycode.tpl</strong> file(s) to customize appearance of each news category.');
+$L['cfg_category'] = array('News categories',
+'News category codes, comma separated. Main (first) news category is displayed on main page using {INDEX_NEWS} tag in index.tpl.<br />
+Additional news categories are displayed on main page using <strong>{INDEX_NEWS_CATEGORYCODE}</strong> tag in index.tpl. Use <strong>news.categorycode.tpl</strong> file(s) to customize appearance of each news category.');
 $L['cfg_maxpages'] = array('Number of recent pages displayed');
-$L['cfg_addpagination'] = array('Enable pagination for additional categories');
+$L['cfg_syncpagination'] = array('Sync pagination');
+
+$L['Maincat']='Main news category';
+$L['Addcat']='Additional news categories';
+$L['NewsCount']='News per page';
+$L['Template']='Template';
+$L['Template_help']='If additional news category template does not exits in {YOUR_SKIN}/plugins folder, system will use main news template';
+$L['Newscat_exists']='This news category has already been chosen. Please choose another one or delete this category';
+$L['Unsetadd']= 'Do not use additional category if it is same as main category';
 
 ?>
