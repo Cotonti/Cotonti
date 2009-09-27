5e35b06cdb928154ded6c878e1688abfcac7e121
Gert Hengeveld <info@ghengeveld.nl>
2009-09-27 11:02:19 +0000

    Deployed forum icon resources

--- system/resources.php
+++ system/resources.php
@@ -89,12 +89,20 @@ $R['admin_icon_users'] = '<img class="icon" src="images/admin/users.gif" alt="" 
 $R['admin_icon_versions'] = '<img class="icon" src="images/admin/versions.gif" alt="" />';
 
 /* Forums */
-$R['frm_icon_posts_moved'] = '<img class="icon" src="skins/'.$skin.'/img/system/posts_moved.gif" alt="" />';
-$R['frm_icon_posts_new_path'] = 'skins/'.$skin.'/img/system/posts_new.gif';
-$R['frm_icon_posts_path'] = 'skins/'.$skin.'/img/system/posts.gif';
+$R['frm_icon_posts'] = '<img class="icon" src="skins/'.$skin.'/img/system/posts.png" alt="" />';
+$R['frm_icon_posts_hot'] = '<img class="icon" src="skins/'.$skin.'/img/system/posts_hot.png" alt="" />';
+$R['frm_icon_posts_locked'] = '<img class="icon" src="skins/'.$skin.'/img/system/posts_locked.png" alt="" />';
+$R['frm_icon_posts_moved'] = '<img class="icon" src="skins/'.$skin.'/img/system/posts_moved.png" alt="" />';
+$R['frm_icon_posts_new'] = '<img class="icon" src="skins/'.$skin.'/img/system/posts_new.png" alt="" />';
+$R['frm_icon_posts_new_hot'] = '<img class="icon" src="skins/'.$skin.'/img/system/posts_new_hot.png" alt="" />';
+$R['frm_icon_posts_new_locked'] = '<img class="icon" src="skins/'.$skin.'/img/system/posts_new_locked.png" alt="" />';
+$R['frm_icon_posts_new_sticky'] = '<img class="icon" src="skins/'.$skin.'/img/system/posts_new_sticky.png" alt="" />';
+$R['frm_icon_posts_new_sticky_locked'] = '<img class="icon" src="skins/'.$skin.'/img/system/posts_new_sticky_locked.png" alt="" />';
+$R['frm_icon_posts_sticky'] = '<img class="icon" src="skins/'.$skin.'/img/system/posts_sticky.png" alt="" />';
+$R['frm_icon_posts_sticky_locked'] = '<img class="icon" src="skins/'.$skin.'/img/system/posts_sticky_locked.png" alt="" />';
 $R['frm_icon_section_activity'] = '<img class="icon" src="skins/'.$skin.'/img/system/activity{$secact_num}.gif" alt="" />';
-$R['frm_icon_topic'] = '<img class="icon" src="skins/'.$skin.'/img/system/{$icon}.gif" alt="" />';
-$R['frm_icon_topic_t'] = '<img class="icon" src="skins/'.$skin.'/img/system/{$icon}.gif" alt="" title="{$title}" />';
+$R['frm_icon_topic'] = '<img class="icon" src="skins/'.$skin.'/img/system/{$icon}.png" alt="" />';
+$R['frm_icon_topic_t'] = '<img class="icon" src="skins/'.$skin.'/img/system/{$icon}.png" alt="" title="{$title}" />';
 
 /* Page */
 $R['page_icon_file'] = '<img class="icon" src="{$icon}" alt="" />';
