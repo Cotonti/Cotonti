f4e5fc081eeab14209fddb4e160f35fda9a843b1
Dmitri Beliavski <support@seditio.by>
2010-01-26 19:03:24 +0000

    Relocate skin lang-files

--- system/lang/en/skin.lang.php
+++ system/lang/en/skin.lang.php
@@ -1,9 +1,9 @@
 <?PHP
 /**
- * English Language File for Sed-Light Skin
+ * English Language File for a Skin
  *
  * @package Cotonti
- * @version 0.1.0
+ * @version 0.6.6
  * @author Cotonti Translators Team
  * @copyright Copyright (c) Cotonti Team 2008-2009
  * @license BSD
