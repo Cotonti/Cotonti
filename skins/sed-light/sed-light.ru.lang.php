5da97e66538868998e2af45e7b4758f6ff53ceb5
Dmitri Beliavski <mail@seditio.by>
2010-01-27 08:56:35 +0000

    Put back empty user skin lang-files

---
+++ skins/sed-light/sed-light.ru.lang.php
@@ -0,0 +1,18 @@
+<?PHP
+/**
+ * Пользовательский файл локализации скина Sed-Light
+ *
+ * @package Cotonti
+ * @version 0.6.6
+ * @author Cotonti Translators Team
+ * @copyright Copyright (c) Cotonti Team 2008-2009
+ * @license BSD
+ */
+
+defined('SED_CODE') or die('Wrong URL.');
+
+/**
+ * Данный файл предназначен для дополнительных пользовательских локализационных строк
+ */
+
+?>
