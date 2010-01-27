ffb6719071d74eddc00ecb64522b620edf48f0ac
Dmitri Beliavski <support@seditio.by>
2010-01-27 08:56:35 +0000

    Put back empty user skin lang-files

---
+++ skins/sed-light/sed-light.en.lang.php
@@ -0,0 +1,19 @@
+<?PHP
+/**
+ * User English Language File for Sed-Light Skin
+ *
+ * @package Cotonti
+ * @version 0.6.6
+ * @author Cotonti Translators Team
+ * @copyright Copyright (c) Cotonti Team 2008-2009
+ * @license BSD
+ */
+
+defined('SED_CODE') or die('Wrong URL.');
+
+/**
+ * Put your skin-specific language strings here
+ */
+
+
+?>
