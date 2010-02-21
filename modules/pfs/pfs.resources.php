75d3ca38a63ec1a49aefdbf9a391d52dfa2d3109
Vladimir Sibirov <trustmaster@kodigy.com>
2010-02-21 16:52:23 +0000

    Added hash-address support for AJAX forms. Fixed a few things.

--- modules/pfs/pfs.resources.php
+++ modules/pfs/pfs.resources.php
@@ -13,7 +13,7 @@ $R['pfs_icon_folder'] = '<img class="icon" src="skins/'.$skin.'/img/system/icon-
 $R['pfs_icon_pastefile'] = '<img class="icon" src="skins/'.$skin.'/img/system/icon-pastefile.gif" title="'.$L['pfs_pastefile'].'" />';
 $R['pfs_icon_pasteimage'] = '<img class="icon" src="skins/'.$skin.'/img/system/icon-pasteimage.gif" title="'.$L['pfs_pasteimage'].'" />';
 $R['pfs_icon_pastethumb'] = '<img class="icon" src="skins/'.$skin.'/img/system/icon-pastethumb.gif" title="'.$L['pfs_pastethumb'].'" />';
-$R['pfs_icon_type'] = '<img class="icon" src="images/pfs/{$type}.gif" alt="{$name}" />';
+$R['pfs_icon_type'] = '<img class="icon" src="'.$cfg['modules_dir'].'/pfs/img/{$type}.gif" alt="{$name}" />';
 
 $R['pfs_link_addfile'] = '<a href="javascript:addfile(\'{$pfs_file}\',\'{$c1}\',\'{$c2}\')">'.$R['pfs_icon_pastefile'].'</a>';
 $R['pfs_link_addpix'] = '<a href="javascript:addpix(\''.$cfg['pfs_path'].'{$pfs_file}\',\'{$c1}\',\'{$c2}\')\">'.$R['pfs_icon_pasteimage'].'</a>';
