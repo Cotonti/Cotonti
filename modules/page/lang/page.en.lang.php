a61f3fba5b376f2cabce53d4600f16425d59b795
Dmitri Beliavski <mail@seditio.by>
2010-02-24 20:05:03 +0000

    Some lang fixes for the Pages Module

--- modules/page/lang/page.en.lang.php
+++ modules/page/lang/page.en.lang.php
@@ -11,11 +11,24 @@
 
 defined('SED_CODE') or die('Wrong URL.');
 
-$L['comm_on_page'] = 'on page';	// New in N-0.0.2
-$L['pagadd_subtitle'] = 'Submit a new page';
-$L['pagadd_title'] = 'New page submission form';
+/**
+ * page.add.tpl
+ */
+
+$L['pagadd_subtitle'] = 'New page submission form';
+$L['pagadd_title'] = 'Submit a new page';
+
+/**
+ * page.edit.tpl
+ */
+
 $L['paged_subtitle'] = 'Update values for this page';
 $L['paged_title'] = 'Page properties';
+
+/**
+ * page.tpl
+ */
+
 $L['pag_authortooshort'] = 'The author name is too short or missing';
 $L['pag_catmissing'] = 'The category code is missing';
 $L['pag_desctooshort'] = 'The description is too short or missing';
@@ -23,5 +36,11 @@ $L['pag_notavailable'] = 'This page will be published in ';	// New in N-0.0.2
 $L['pag_titletooshort'] = 'The title is too short or missing';
 $L['pag_validation'] = 'Awaiting validation';
 $L['pag_validation_desc'] = 'Your pages which have not been validated by administrator yet';
+
+/**
+ * elsewhere?
+ */
+
+$L['comm_on_page'] = 'on page';	// New in N-0.0.2
 
 ?>
--- modules/page/lang/page.ru.lang.php
+++ modules/page/lang/page.ru.lang.php
@@ -11,11 +11,24 @@
 
 defined('SED_CODE') or die('Wrong URL.');
 
-$L['comm_on_page'] = 'на странице'; // New in N-0.0.2
+/**
+ * page.add.tpl
+ */
+
 $L['pagadd_subtitle'] = 'Форма для создания новой страницы';
-$L['pagadd_title'] = 'Новая страница';
+$L['pagadd_title'] = 'Создать новую страницу';
+
+/**
+ * page.edit.tpl
+ */
+
 $L['paged_subtitle'] = 'Форма для редактирования страницы';
 $L['paged_title'] = 'Свойства страницы';
+
+/**
+ * page.tpl
+ */
+
 $L['pag_authortooshort'] = 'Имя автора слишком короткое либо отсутствует';
 $L['pag_catmissing'] = 'Код категории отсутствует';
 $L['pag_desctooshort'] = 'Описание слишком короткое либо отсутствует';
@@ -23,5 +36,11 @@ $L['pag_notavailable'] = 'Страница будет опубликована '
 $L['pag_titletooshort'] = 'Заголовок слишком короткий либо отсутствует';
 $L['pag_validation'] = 'Ожидают утверждения';
 $L['pag_validation_desc'] = 'Ваши страницы, которые не были ещё утверждены администратором';
+
+/**
+ * elsewhere?
+ */
+
+$L['comm_on_page'] = 'на странице'; // New in N-0.0.2
 
 ?>
