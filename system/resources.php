d59fa41e39de59ae92304e02ef477a4e1f8ad61d
Dmitri Beliavski <mail@seditio.by>
2010-03-15 08:40:19 +0000

    Move icons to default iconpack

--- system/resources.php
+++ system/resources.php
@@ -69,7 +69,45 @@ $R['form_guest_username'] = '<input type="text" name="rusername" size="12" maxle
  */
 
 $R['icon_rss'] = '<img class="icon" src="images/iconpacks/default/rss.png" alt="" />';
+$R['icon_twitter'] = '<img class="icon" src="images/iconpacks/default/twitter.png" alt="" />';
 $R['img_pixel'] = '<img src="images/pixel.gif" width="{$x}" height="{$y}" alt="" />';
 $R['link_catpath'] = '<a href="{$url}" title="{$title}">{$title}</a>';
+
+/**
+ * Temporary
+ */
+
+$R['icon_error'] = 
+	'<img class="icon" src="images/iconpacks/default/error.png" alt="" />';
+$R['icon_forums'] = 
+	'<img class="icon" src="images/iconpacks/default/forums.png" alt="" />';
+$R['icon_help'] = 
+	'<img class="icon" src="images/iconpacks/default/help.png" alt="" />';
+$R['icon_info'] = 
+	'<img class="icon" src="images/iconpacks/default/info.png" alt="" />';
+$R['icon_news'] = 
+	'<img class="icon" src="images/iconpacks/default/news.png" alt="" />';
+$R['icon_online'] = 
+	'<img class="icon" src="images/iconpacks/default/online.png" alt="" />';
+$R['icon_pfs'] = 
+	'<img class="icon" src="images/iconpacks/default/pfs.png" alt="" />';
+$R['icon_plugin'] = 
+	'<img class="icon" src="images/iconpacks/default/plugin.png" alt="" />';
+$R['icon_polls'] = 
+	'<img class="icon" src="images/iconpacks/default/polls.png" alt="" />';
+$R['icon_prefs'] = 
+	'<img class="icon" src="images/iconpacks/default/prefs.png" alt="" />';
+$R['icon_search'] = 
+	'<img class="icon" src="images/iconpacks/default/search.png" alt="" />';
+$R['icon_stats'] = 
+	'<img class="icon" src="images/iconpacks/default/stats.png" alt="" />';
+$R['icon_tags'] = 
+	'<img class="icon" src="images/iconpacks/default/tags.png" alt="" />';
+$R['icon_update'] = 
+	'<img class="icon" src="images/iconpacks/default/update.png" alt="" />';
+$R['icon_users'] = 
+	'<img class="icon" src="images/iconpacks/default/users.png" alt="" />';
+$R['icon_warning'] = 
+	'<img class="icon" src="images/iconpacks/default/warning.png" alt="" />';
 
 ?>
