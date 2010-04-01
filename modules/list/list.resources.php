8dc9a2daaee8e9d178a2d22a6985f31dcb31606b
Dr2005alex <dr2005alex@mail.ru>
2010-04-01 19:26:05 +0000

    Move HTML from list into resources

--- modules/list/list.resources.php
+++ modules/list/list.resources.php
@@ -14,22 +14,22 @@ $R['page_icon_file_path'] = 'images/filetypes/default/{$type}.png';
 
 $R['page_submitnewpage'] = '<a href="{$sub_url}" />'.$L['Submitnew'].'</a>';
 
-$R['list_top_title_down'] = '<a href="{$list_top_url_down}" />{$sed_img_down}</a>';
-$R['list_top_title_up'] = '<a href="{$list_top_url_up}" />{$sed_img_up}</a> '.$L['Title'];
-$R['list_top_key_down'] = '<a href="{$list_top_key_url_down}" />{$sed_img_down}</a>';
-$R['list_top_key_up'] = '<a href="{$list_top_key_url_up}" />{$sed_img_up}</a> '.$L['Key'];
-$R['list_top_date_down'] = '<a href="{$list_top_date_url_down}" />{$sed_img_down}</a>';
-$R['list_top_date_up'] = '<a href="{$list_top_date_url_up}" />{$sed_img_up}</a> '.$L['Date'];
-$R['list_top_author_down'] = '<a href="{$list_top_author_url_down}" />{$sed_img_down}</a>';
-$R['list_top_author_up'] = '<a href="{$list_top_author_url_up}" />{$sed_img_up}</a> '.$L['Author'];
-$R['list_top_owner_down'] = '<a href="{$list_top_owner_url_down}" />{$sed_img_down}</a>';
-$R['list_top_owner_up'] = '<a href="{$list_top_owner_url_up}" />{$sed_img_up}</a> '.$L['Owner'];
-$R['list_top_count_down'] = '<a href="{$list_top_count_url_down}" />{$sed_img_down}</a>';
-$R['list_top_count_up'] = '<a href="{$list_top_count_url_up}" />{$sed_img_up}</a> '.$L['Hits'];
-$R['list_top_filecount_down'] = '<a href="{$list_top_filecount_url_down}" />{$sed_img_down}</a>';
-$R['list_top_filecount_up'] = '<a href="{$list_top_filecount_url_up}" />{$sed_img_up}</a> '.$L['Hits'];
-$R['list_top_field_name_down'] = '<a href="{$list_top_field_name_url_down}" />{$sed_img_down}</a>';
-$R['list_top_field_name_up'] = '<a href="{$list_top_field_name_url_up}" />{$sed_img_up}</a> '.$extratitle;
+$R['list_top_title'] = '<a href="{$list_top_url_down}" />{$sed_img_down}</a>';
+$R['list_top_title'].= '<a href="{$list_top_url_up}" />{$sed_img_up}</a> '.$L['Title'];
+$R['list_top_key'] = '<a href="{$list_top_key_url_down}" />{$sed_img_down}</a>';
+$R['list_top_key'].= '<a href="{$list_top_key_url_up}" />{$sed_img_up}</a> '.$L['Key'];
+$R['list_top_date'] = '<a href="{$list_top_date_url_down}" />{$sed_img_down}</a>';
+$R['list_top_date'].= '<a href="{$list_top_date_url_up}" />{$sed_img_up}</a> '.$L['Date'];
+$R['list_top_author'] = '<a href="{$list_top_author_url_down}" />{$sed_img_down}</a>';
+$R['list_top_author'].= '<a href="{$list_top_author_url_up}" />{$sed_img_up}</a> '.$L['Author'];
+$R['list_top_owner'] = '<a href="{$list_top_owner_url_down}" />{$sed_img_down}</a>';
+$R['list_top_owner'].= '<a href="{$list_top_owner_url_up}" />{$sed_img_up}</a> '.$L['Owner'];
+$R['list_top_count'] = '<a href="{$list_top_count_url_down}" />{$sed_img_down}</a>';
+$R['list_top_count'].= '<a href="{$list_top_count_url_up}" />{$sed_img_up}</a> '.$L['Hits'];
+$R['list_top_filecount'] = '<a href="{$list_top_filecount_url_down}" />{$sed_img_down}</a>';
+$R['list_top_filecount'].= '<a href="{$list_top_filecount_url_up}" />{$sed_img_up}</a> '.$L['Hits'];
+$R['list_top_field_name'] = '<a href="{$list_top_field_name_url_down}" />{$sed_img_down}</a>';
+$R['list_top_field_name'].= '<a href="{$list_top_field_name_url_up}" />{$sed_img_up}</a> '.$extratitle;
 
 $R['list_row_admin'] = '<a href="{$unvalidate_url}" />'.$L['Putinvalidationqueue'].'</a> &nbsp;<a href="{$edit_url}" />'.$L['Edit'].'</a>';
 
