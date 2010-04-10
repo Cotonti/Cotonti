48726da23446c8c91c2f4bb58e94ce37409b5179
Dmitri Beliavski <support@seditio.by>
2010-04-10 18:53:27 +0000

    Finalized recentitems & news plugin templates, skin minor updates, added new icons in the default iconset

--- images/icons/default/resources.php
+++ images/icons/default/resources.php
@@ -34,10 +34,13 @@ $R['icon_twitter'] = '<img class="icon" src="images/icons/default/twitter.png" a
  */
 
 $R['icon_comments'] = '<img class="icon" src="images/icons/default/comments.png" alt="" />';
+$R['icon_delete'] = '<img class="icon" src="images/icons/default/delete.png" alt="" />';
 $R['icon_error'] = '<img class="icon" src="images/icons/default/error.png" alt="" />';
 $R['icon_forums'] = '<img class="icon" src="images/icons/default/forums.png" alt="" />';
 $R['icon_help'] = '<img class="icon" src="images/icons/default/help.png" alt="" />';
 $R['icon_info'] = '<img class="icon" src="images/icons/default/info.png" alt="" />';
+$R['icon_jumpto'] = '<img class="icon" src="images/icons/default/arrow-jump.png" alt="" />';
+$R['icon_lock'] = '<img class="icon" src="images/icons/default/lock.png" alt="" />';
 $R['icon_news'] = '<img class="icon" src="images/icons/default/news.png" alt="" />';
 $R['icon_online'] = '<img class="icon" src="images/icons/default/online.png" alt="" />';
 $R['icon_page'] = '<img class="icon" src="images/icons/default/page.png" alt="" />';
@@ -45,6 +48,7 @@ $R['icon_pfs'] = '<img class="icon" src="images/icons/default/pfs.png" alt="" />
 $R['icon_plugin'] = '<img class="icon" src="images/icons/default/plugin.png" alt="" />';
 $R['icon_polls'] = '<img class="icon" src="images/icons/default/polls.png" alt="" />';
 $R['icon_prefs'] = '<img class="icon" src="images/icons/default/prefs.png" alt="" />';
+$R['icon_reset'] = '<img class="icon" src="images/icons/default/reset.png" alt="" />';
 $R['icon_search'] = '<img class="icon" src="images/icons/default/search.png" alt="" />';
 $R['icon_stats'] = '<img class="icon" src="images/icons/default/stats.png" alt="" />';
 $R['icon_tags'] = '<img class="icon" src="images/icons/default/tags.png" alt="" />';
