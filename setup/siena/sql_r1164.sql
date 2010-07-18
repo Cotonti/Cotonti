db1d05cb5afb1f7480c805c416c41b4349359843
Vladimir Sibirov <trustmaster@kodigy.com>
2010-07-18 09:43:35 +0000

    Backticks are required for correct table prefix handling

--- setup/siena/sql_r1164.sql
+++ setup/siena/sql_r1164.sql
@@ -1,7 +1,7 @@
 /* r1164  Update settings from search plugin */
-DELETE FROM sed_config WHERE config_owner = 'plug' AND config_cat = 'search' AND config_name = 'maxitems_ext';
-DELETE FROM sed_config WHERE config_owner = 'plug' AND config_cat = 'search' AND config_name = 'showtext_ext';
-DELETE FROM sed_config WHERE config_owner = 'plug' AND config_cat = 'search' AND config_name = 'showtext';
+DELETE FROM `sed_config` WHERE config_owner = 'plug' AND config_cat = 'search' AND config_name = 'maxitems_ext';
+DELETE FROM `sed_config` WHERE config_owner = 'plug' AND config_cat = 'search' AND config_name = 'showtext_ext';
+DELETE FROM `sed_config` WHERE config_owner = 'plug' AND config_cat = 'search' AND config_name = 'showtext';
 
 INSERT INTO `sed_config` VALUES ('plug', 'search', '5', 'pagesearch', 3, '1', '1', '', 'Enable pages search');
 INSERT INTO `sed_config` VALUES ('plug', 'search', '6', 'forumsearch', 3, '1', '1', '', 'Enable forums search');
--- setup/siena/sql_r1252.sql
+++ setup/siena/sql_r1252.sql
@@ -1,7 +1,7 @@
 /* Plugin extension */
-ALTER TABLE sed_plugins ADD COLUMN `pl_module` tinyint(1) unsigned NOT NULL
+ALTER TABLE `sed_plugins` ADD COLUMN `pl_module` tinyint(1) unsigned NOT NULL
     DEFAULT 0;
 
 /* Obsolete entries removal */
-DELETE FROM sed_config WHERE config_owner = 'core' AND config_cat = 'skin' AND
+DELETE FROM `sed_config` WHERE config_owner = 'core' AND config_cat = 'skin' AND
     config_name = 'doctypeid';
