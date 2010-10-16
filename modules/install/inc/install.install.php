9eb9c239a0b0a5ca56e51fd8369d2cf9556a22e9
Vladimir Sibirov <trustmaster@kodigy.com>
2010-10-16 11:46:41 +0000

    Default plugin selection change

--- modules/install/inc/install.install.php
+++ modules/install/inc/install.install.php
@@ -11,7 +11,7 @@ defined('COT_CODE') or die('Wrong URL');
 
 // Modules and plugins checked by default
 $default_modules = array('index', 'page', 'rss');
-$default_plugins = array('cleaner', 'ipsearch', 'markitup', 'news');
+$default_plugins = array('ckeditor', 'cleaner', 'htmlpurifier', 'ipsearch', 'news');
 
 $step = empty($_SESSION['cot_inst_lang']) ? 0 : (int) $cfg['new_install'];
 
