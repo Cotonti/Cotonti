dfe95ddeea56695a77af1a1024b5dd5eadb21e46
Gert Hengeveld <info@ghengeveld.nl>
2010-11-02 01:58:35 +0000

    #559 Add language file

---
+++ plugins/hiddengroups/lang/hiddengroups.en.lang.php
@@ -0,0 +1,7 @@
+<?php
+
+defined('COT_CODE') or die('Wrong URL');
+
+$L['cfg_mode'] = array('Hiding mode', 'Whether or not to hide users in userlist/whosonline when their maingroup or any of their subgroups is hidden.');
+
+?>
