c1de7a892cebb6460939ffb819a5bd4f85fffe53
Dmitri Beliavski <support@seditio.by>
2010-12-16 08:01:16 +0000

    Sync changes from userimages

--- plugins/userimages/lang/userimages.ru.lang.php
+++ plugins/userimages/lang/userimages.ru.lang.php
@@ -1,26 +1,32 @@
 <?php
+/**
+ * English Language File for Banlist
+ *
+ * @package Userimages
+ * @version 
+ * @author Cotonti Translators Team
+ * @copyright Copyright (c) Cotonti Team 2008-2010
+ * @license BSD
+ */
 
 defined('COT_CODE') or die('Wrong URL.');
 
-$L['Code'] = 'Code';
-$L['Width'] = 'Width';
-$L['Height'] = 'Height';
-$L['CropRatio'] = 'Crop ratio';
+$L['userimages_width'] = 'Width';
+$L['userimages_height'] = 'Height';
+$L['userimages_cropratio'] = 'Crop ratio';
+$L['userimages_cropnone'] = 'None (original width:height)';
+$L['userimages_cropfit'] = 'Fit (target width:height)';
 
-$L['AddNew'] = 'Add new';
-$L['CropNone'] = 'None (original width:height)';
-$L['CropFit'] = 'Fit (target width:height)';
+$L['userimages_avatarnotvalid'] = 'This avatar is not a valid %1$s image.';
+$L['userimages_avatarschoose'] = 'Click an image below to set it as your own avatar';
+$L['userimages_avatarspreset'] = '...or click here to display a gallery of pre-loaded avatars';
+$L['userimages_avatarsupload'] = 'Upload an avatar';
+$L['userimages_avataruploadfailed'] = 'The upload failed, delete the old avatar before to free the slot!';
+$L['userimages_photonotvalid'] = 'This photo is not a valid %1$s image.';
+$L['userimages_photoupload'] = 'Upload a photography';
+$L['userimages_sigotvalid'] = 'This signature is not a valid %1$s image.';
+$L['userimages_sigupload'] = 'Upload a signature';
 
 $L['userimages_help'] = '';
-
-$L['pro_avatarnotvalid'] = 'Этот аватар не является %1$s-файлом.';	// New in 0.0.2
-$L['pro_avatarschoose'] = 'В качестве своего аватара выберите следующие изображения';
-$L['pro_avatarspreset'] = '...или выбрать аватар из списка доступных';
-$L['pro_avatarsupload'] = 'Загрузить аватар';
-$L['pro_avataruploadfailed'] = 'Ошибка загрузки: удалите старый аватар!';
-$L['pro_photonotvalid'] = 'Это фото не является %1$s-файлом.';	// New in 0.0.2
-$L['pro_photoupload'] = 'Загрузить фото';
-$L['pro_sigotvalid'] = 'Эта подпись не является %1$s-файлом.';	// New in 0.0.2
-$L['pro_sigupload'] = 'Загрузить подпись';
 
 ?>
