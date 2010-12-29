a79d2e318aaf3501ff739e18d5f784940495bd6b
Vladimir Sibirov <trustmaster@kodigy.com>
2010-12-29 20:06:21 +0000

    Fixed view.php

--- view.php
+++ view.php
@@ -30,25 +30,25 @@ if (mb_strpos($v, '.') !== false || mb_strpos($v, '/') !== false)
 	die('Wrong URL.');
 }
 
-$incl_html = "datas/html/".$v.".html";
-$incl_htm = "datas/html/".$v.".htm";
-$incl_txt = "datas/html/".$v.".txt";
+$incl_html = "datas/html/" . $v . ".html";
+$incl_htm = "datas/html/" . $v . ".htm";
+$incl_txt = "datas/html/" . $v . ".txt";
 
 if (file_exists($incl_txt))
 {
-	$fd = @fopen($incl_txt, 'r') or die("Couldn't find a file : ".$incl_txt); // TODO: Need translate
+	$fd = @fopen($incl_txt, 'r') or die("Couldn't find a file : " . $incl_txt); // TODO: Need translate
 	$vd = fread($fd, filesize($incl_txt));
 	fclose($fd);
 }
 elseif (file_exists($incl_htm))
 {
-	$fd = @fopen($incl_htm, 'r') or die("Couldn't find a file : ".$incl_htm); // TODO: Need translate
+	$fd = @fopen($incl_htm, 'r') or die("Couldn't find a file : " . $incl_htm); // TODO: Need translate
 	$vd = fread($fd, filesize($incl_htm));
 	fclose($fd);
 }
 elseif (file_exists($incl_html))
 {
-	$fd = @fopen($incl_html, 'r') or die("Couldn't find a file : ".$incl_html); // TODO: Need translate
+	$fd = @fopen($incl_html, 'r') or die("Couldn't find a file : " . $incl_html); // TODO: Need translate
 	$vd = fread($fd, filesize($incl_html));
 	fclose($fd);
 }
@@ -61,10 +61,18 @@ if (preg_match('@<head>(.*?)</head>@si', $vd, $ext_head) == 1)
 {
 	$ext_head = $ext_head[1];
 }
+else
+{
+	$ext_head = '';
+}
 
 if (preg_match('@<body[^>]*?>(.*?)</body>@si', $vd, $ext_body) == 1)
 {
 	$ext_body = $ext_body[1];
+}
+else
+{
+	$ext_body = $vd;
 }
 
 $vt = '&nbsp;';
@@ -77,16 +85,16 @@ if (mb_stripos($ext_head, '<meta name="cot_title"') !== false)
 	$tag_title_end = mb_strpos($vt, '">');
 	$vt = mb_substr($vt, 0, $tag_title_end);
 }
-elseif (preg_match('@<title>(.*?)</title>@si', $ext_head, $vt)==1)
+elseif (preg_match('@<title>(.*?)</title>@si', $ext_head, $vt) == 1)
 {
 	$vt = $vt[1];
 }
 
-if (preg_match_all('@<script[^>]*?>(.*?)</script>@si',$ext_head,$ext_js) > 0)
+if (preg_match_all('@<script[^>]*?>(.*?)</script>@si', $ext_head, $ext_js) > 0)
 {
 	foreach ($ext_js[1] as $js)
 	{
-		$js = preg_replace(array('@<!--(.*?)\n@si','@\/\/(.*?)-->\n@si'),array('',''),$js);
+		$js = preg_replace(array('@<!--(.*?)\n@si', '@\/\/(.*?)-->\n@si'), array('', ''), $js);
 		$morejavascript .= $js;
 	}
 }
@@ -99,7 +107,7 @@ if (preg_match_all('@<link[^>](.*?)>@si', $ext_head, $ext_links) > 0)
 	}
 }
 
-require_once $cfg['system_dir'].'/header.php';
+require_once $cfg['system_dir'] . '/header.php';
 $t = new XTemplate(cot_tplfile('plugin'));
 
 $t->assign(array(
@@ -110,6 +118,6 @@ $t->assign(array(
 $t->parse("MAIN");
 $t->out("MAIN");
 
-require_once $cfg['system_dir'].'/footer.php';
+require_once $cfg['system_dir'] . '/footer.php';
 
 ?>
