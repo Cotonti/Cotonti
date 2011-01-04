8ae93ea4362cd54e31a9e0499ebb9aa408046a86
Vladimir Sibirov <trustmaster@kodigy.com>
2011-01-04 13:52:04 +0000

    A fix for page validation queue

--- modules/page/page.admin.php
+++ modules/page/page.admin.php
@@ -23,6 +23,7 @@ cot_block($usr['isadmin']);
 $t = new XTemplate(cot_tplfile('page.admin', 'module'));
 
 require_once cot_incfile('page', 'module');
+require_once cot_incfile('users', 'module');
 
 $adminpath[] = array(cot_url('admin', 'm=page'), $L['Pages']);
 $adminhelp = $L['adm_help_page'];
