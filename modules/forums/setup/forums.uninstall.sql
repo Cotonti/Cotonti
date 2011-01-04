669de96009b3987f03228d41b8994aaf6533c715
Pavel Mikulik <esclkm@gmail.com>
2011-01-04 16:27:51 +0000

    several forums fix

--- modules/forums/setup/forums.uninstall.sql
+++ modules/forums/setup/forums.uninstall.sql
@@ -5,3 +5,6 @@
 DROP TABLE IF EXISTS `cot_forum_posts`;
 DROP TABLE IF EXISTS `cot_forum_topics`;
 DROP TABLE IF EXISTS `cot_forum_stats`;
+
+DELETE FROM `cot_auth` WHERE `auth_code` = 'forums';
+DELETE FROM `cot_structure` WHERE `structure_area` = 'forums';
