6420ad3cf9e81c8a0514b0a6e36cc9847a979009
Pavel Mikulik <esclkm@gmail.com>
2011-01-09 12:48:49 +0000

    lang strings fixed

--- plugins/search/search.php
+++ plugins/search/search.php
@@ -290,7 +290,7 @@ if (!empty($sq))
 	if (($tab == 'frm' || empty($tab)) && cot_module_active('forums') && $cfg['plugin']['search']['forumsearch'] && !cot_error_found())
 	{
 		$where_and['cat'] = ($rsearch['frm']['sub'][0] != 'all' && count($rsearch['frm']['sub'])>0) ?
-			"s.fs_id IN ('".$db->prep(implode("','", $rsearch['frm']['sub']))."')" : "s.fs_id IN ('".implode("','", $frm_catauth)."')";
+			"s.ft_cat IN ('".$db->prep(implode("','", $rsearch['frm']['sub']))."')" : "s.ft_cat IN ('".implode("','", $frm_catauth)."')";
 		$where_and['reply'] = ($rsearch['frm']['reply'] == '1') ? "t.ft_postcount > 1" : "";
 		$where_and['time'] = ($rsearch['set']['limit'] > 0) ? "p.fp_creation >= ".$rsearch['set']['from']." AND p.fp_updated <= ".$rsearch['set']['to'] : "";
 		$where_and['user'] = (!empty($touser)) ? "p.fp_posterid ".$touser_ids : "";
