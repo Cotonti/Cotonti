52340f42c2d79ed896159fd25c7466b180d762b8
Vladimir Sibirov <trustmaster@kodigy.com>
2011-02-06 09:51:15 +0000

    Moved module fields and to modules (#596). Fixed r1813 patch. Fixed admin.users template extension parts.

---
+++ modules/forums/forums.install.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Installation handler
+ *
+ * @package forums
+ * @version 0.7.0
+ * @author Trustmaster
+ * @copyright Copyright (c) Cotonti Team 2011
+ * @license BSD License
+ */
+
+defined('COT_CODE') or die('Wrong URL');
+
+global $db_users;
+
+// Add users fields if missing
+$dbres = $db->query("SHOW COLUMNS FROM `$db_users` WHERE `Field` = 'user_postcount'");
+if ($dbres->rowCount() == 0)
+{
+	$db->query("ALTER TABLE `$db_users` ADD COLUMN `user_postcount` INT DEFAULT 0");
+}
+$dbres->closeCursor();
+
+?>
---
+++ modules/pfs/pfs.admin.users.edit.tags.php
@@ -0,0 +1,27 @@
+<?php
+/* ====================
+[BEGIN_COT_EXT]
+Hooks=admin.users.edit.tags
+[END_COT_EXT]
+==================== */
+
+/**
+ * Users admin edit tags
+ *
+ * @package pfs
+ * @version 0.9.0
+ * @author Cotonti Team
+ * @copyright Copyright (c) Cotonti Team 2011
+ * @license BSD
+ */
+
+defined('COT_CODE') or die('Wrong URL.');
+
+$pfs_is_active = true;
+
+$t->assign(array(
+	'ADMIN_USERS_EDITFORM_GRP_PFS_MAXFILE' => cot_inputbox('text', 'rmaxfile', htmlspecialchars($row['grp_pfs_maxfile']), 'size="16" maxlength="16"'),
+	'ADMIN_USERS_EDITFORM_GRP_PFS_MAXTOTAL' => cot_inputbox('text', 'rmaxtotal', htmlspecialchars($row['grp_pfs_maxtotal']), 'size="16" maxlength="16"')
+));
+
+?>
