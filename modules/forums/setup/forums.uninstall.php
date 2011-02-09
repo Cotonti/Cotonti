ee5ebb1e7e6bcb60fed89c1314bf9991b1b16e4c
Vladimir Sibirov <trustmaster@kodigy.com>
2011-02-09 14:04:40 +0000

    small bugfix

--- modules/forums/setup/forums.uninstall.php
+++ modules/forums/setup/forums.uninstall.php
@@ -13,8 +13,8 @@ defined('COT_CODE') or die('Wrong URL');
 
 global $db_users;
 
-// Remove PM columns from users table
-$dbres = $db->query("SHOW COLUMNS FROM `$db_users` WHERE `Field` = 'user_pmnotify'");
+// Remove forums columns from users table
+$dbres = $db->query("SHOW COLUMNS FROM `$db_users` WHERE `Field` = 'user_postcount'");
 if ($dbres->rowCount() == 1)
 {
 	$db->query("ALTER TABLE `$db_users` DROP COLUMN `user_postcount`");
