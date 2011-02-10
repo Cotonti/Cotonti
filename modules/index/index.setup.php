e54583d68fbbb3cf0d543c7a3b0a511bf0dfcdb4
Gert Hengeveld <koradhil@gmail.com>
2011-02-10 15:41:03 +0000

    Revert index module to r1471, in order to allow rights configuration for index. Also added cot_block to index, in case visitor doesnt have read rights.

---
+++ modules/index/index.setup.php
@@ -0,0 +1,30 @@
+<?php
+/* ====================
+[BEGIN_COT_EXT]
+Name=Index
+Description=Home Page
+Version=0.9.0
+Date=2010-02-10
+Author=Cotonti Team
+Copyright=(c) Cotonti Team 2008-2011
+Notes=BSD License
+Auth_guests=R
+Lock_guests=A
+Auth_members=R
+Lock_members=
+Recommends_modules=page
+Recommends_plugins=news,recentitems
+[END_COT_EXT]
+==================== */
+
+/**
+ * Index module setup file
+ *
+ * @package index
+ * @version 0.9.0
+ * @author Cotonti Team
+ * @copyright Copyright (c) Cotonti Team 2008-2011
+ * @license BSD
+ */
+
+?>
