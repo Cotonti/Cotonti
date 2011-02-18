bcdbb57c75470efe17352c755d845af28efa9c3b
Gert Hengeveld <info@ghengeveld.nl>
2011-02-18 11:39:29 +0000

    Fix missing userlist filter by first letter. Moved it to a plugin.

---
+++ plugins/usersbyfirstletter/lang/usersbyfirstletter.en.lang.php
@@ -0,0 +1,7 @@
+<?php
+
+defined('COT_CODE') or die('Wrong URL');
+
+$L['by_first_letter'] = 'By first letter';
+
+?>
---
+++ plugins/usersbyfirstletter/usersbyfirstletter.setup.php
@@ -0,0 +1,23 @@
+<?php
+
+/* ====================
+[BEGIN_COT_EXT]
+Code=usersbyfirstletter
+Name=Users by first letter
+Description=Filter users in userlist by first letter
+Version=0.9.0
+Date=2011-02-18
+Author=Koradhil
+Copyright=Copyright (c) Cotonti Team 2008-2011
+Notes=BSD License
+SQL=
+Auth_guests=R
+Lock_guests=W12345A
+Auth_members=R
+Lock_members=W12345A
+[END_COT_EXT]
+==================== */
+
+defined('COT_CODE') or die('Wrong URL');
+
+?>
