13f79d50ad4fe830de641dc1e25565fadad487cd
Pavel Mikulik <esclkm@gmail.com>
2011-02-28 08:52:36 +0000

    Fatal error: Call to a member function fetch() on a non-object in /htdocs/modules/forums/inc/forums.posts.php on line 47 - by Alex300 fixed

--- modules/forums/inc/forums.posts.php
+++ modules/forums/inc/forums.posts.php
@@ -27,9 +27,9 @@ foreach (cot_getextplugins('forums.posts.first') as $pl)
 	include $pl;
 }
 /* ===== */
-if (!empty($n) || !empty($p) || !empty($id))
+if ((!empty($n) && !empty($q)) || !empty($p) || !empty($id))
 {
-	if (!empty($q) && ($n == 'last' || ($n == 'unread' && $usr['id'] > 0)))
+	if (!empty($q) && ($n == 'last' || ($n == 'unread' && $usr['id'] == 0)))
 	{
 		$sql_forums = $db->query("SELECT fp_id, fp_topicid, fp_cat, fp_posterid FROM $db_forum_posts
 			WHERE fp_topicid = $q ORDER by fp_id DESC LIMIT 1");
@@ -44,7 +44,7 @@ if (!empty($n) || !empty($p) || !empty($id))
 		$p = ($p > 0) ? $p : $id;
 		$sql_forums = $db->query("SELECT fp_id, fp_topicid, fp_cat, fp_posterid FROM $db_forum_posts WHERE fp_id = $p LIMIT 1");
 	}
-	if ($row = $sql_forums->fetch())
+	if (isset($sql_forums) && $row = $sql_forums->fetch())
 	{
 		$p = $row['fp_id'];
 		$q = $row['fp_topicid'];
