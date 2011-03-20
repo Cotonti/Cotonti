5669e5bb2630d2ed46169c39af7e8497e7e30e43
Vladimir Sibirov <trustmaster@kodigy.com>
2011-03-20 13:21:31 +0000

    Fix for #623 by dervan

--- system/users/users.main.php
+++ system/users/users.main.php
@@ -228,6 +228,7 @@ $t->assign(array(
 	'USERS_TOP_PM' => 'PM',
 ));
 
+$k = '_.__._';
 $asc = explode($k, cot_url('users', array('s' => $k, 'w'=> 'asc') + $users_url_path));
 $desc = explode($k, cot_url('users', array('s' => $k, 'w'=> 'desc') + $users_url_path));
 foreach ($users_sort_tags as $k => $x)
