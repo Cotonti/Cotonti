ef26b064c014457e9950fa011a98edcf66bd5b53
Vladimir Sibirov <trustmaster@kodigy.com>
2011-04-02 08:58:54 +0000

    CKEditor code paste fixes

--- plugins/ckeditor/lib/plugins/code/dialogs/code.js
+++ plugins/ckeditor/lib/plugins/code/dialogs/code.js
@@ -1,3 +1,12 @@
+function escapeHtml(unsafe) {
+  return unsafe
+      .replace(/&/g, "&amp;")
+      .replace(/</g, "&lt;")
+      .replace(/>/g, "&gt;")
+      .replace(/"/g, "&quot;")
+      .replace(/'/g, "&#039;");
+}
+
 CKEDITOR.dialog.add( 'code', function( editor )
 {
 	return {
@@ -5,45 +14,54 @@ CKEDITOR.dialog.add( 'code', function( editor )
 		minWidth : 400,
 		minHeight : 200,
 		contents : [
-			{
-				id : 'tab1',
-				label : 'First Tab',
-				title : 'First Tab',
-				elements :
-				[ { type : 'html',
-					id : 'content',
-					html :'<select size="1" name="chili">'+
-					'<option value="code">Code</option>'+
-					'<option value="php">PHP</option>'+
-					'<option value="php-f">PHP + HTML</option>'+
-					'<option value="mysql">MySQL</option>'+
-					'<option value="lotusscript">LotusScript</option>'+
-					'<option value="js">JavaScript</option>'+
-					'<option value="java">Java</option>'+
-					'<option value="html">HTML</option>'+
-					'<option value="delphi">Delphi</option>'+
-					'<option value="css">CSS</option>'+
-					'<option value="csharp">C#</option>'+
-					'<option value="cplusplus">C++</option>'+
-					'</select>',
-					validate : function(){
+		{
+			id : 'tab1',
+			label : 'First Tab',
+			title : 'First Tab',
+			elements :
+			[ {
+				type : 'html',
+				id : 'content',
+				html :'<select size="1" name="chili">'+
+				'<option value="code">Code</option>'+
+				'<option value="php">PHP</option>'+
+				'<option value="php-f">PHP + HTML</option>'+
+				'<option value="mysql">MySQL</option>'+
+				'<option value="lotusscript">LotusScript</option>'+
+				'<option value="js">JavaScript</option>'+
+				'<option value="java">Java</option>'+
+				'<option value="html">HTML</option>'+
+				'<option value="delphi">Delphi</option>'+
+				'<option value="css">CSS</option>'+
+				'<option value="csharp">C#</option>'+
+				'<option value="cplusplus">C++</option>'+
+				'</select>',
+				validate : function(){
 					CKEDITOR.config.chili_val= this.getValue();
-}},{
-id : 'input1',
-type : 'textarea',
-label : '',
-validate : function()
-{ if ( !this.getValue() )
-{alert( 'Empty code' );
-return false;}
-if(CKEDITOR.config.chili_val=='code'){
-	var txt = '<div class="code">'+ this.getValue().replace(/([^>])\n/g, '$1<br/>') +'</div>';
-}else{
-	var txt = '<div class="highlight"><pre class="'+ CKEDITOR.config.chili_val + '">'+ this.getValue().replace(/([^>])\n/g, '$1<br/>') +'</pre></div>';
-}
-//editor.insertElement( element );
-editor.insertHtml(txt)
-CKEDITOR.ENTER_BR;
-return true;
-}}]}]};
+				}
+			},{
+			id : 'input1',
+			type : 'textarea',
+			label : '',
+			validate : function()
+			{
+				if ( !this.getValue() )
+				{
+					alert( 'Empty code' );
+					return false;
+				}
+				var txt = '';
+				if(CKEDITOR.config.chili_val=='code'){
+					txt = '<div class="code"><pre>'+ escapeHtml(this.getValue()) +'</pre></div>';
+				}else{
+					txt = '<div class="highlight"><pre class="'+ CKEDITOR.config.chili_val + '">'+ escapeHtml(this.getValue()) +'</pre></div>';
+				}
+				//editor.insertElement( element );
+				editor.insertHtml(txt);
+				//CKEDITOR.ENTER_BR;
+				return true;
+			}
+		}]
+	}]
+};
 } );
