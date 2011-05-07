592a2e2d272c51b8cde7e701cd0741d2b2925a7b
Vladimir Sibirov <trustmaster@kodigy.com>
2011-05-07 19:03:53 +0000

    Fixed #662

--- js/base.js
+++ js/base.js
@@ -191,7 +191,7 @@ function ajaxMakeHash(href, rel, formData) {
 	var hash = (formData == 'post') ? 'post' : 'get';
 	var hrefBase, params;
 	var sep = '?';
-	var m = rel.match(/(get|post)(-[^ ;]+)?(;\S*)?$/);
+	var m = rel.match(/(get|post)(-[^ ;]+)?(;\S*)?/);
 	if (m) {
 		hash = m[1];
 		if (m[2]) {
