dc6e0742692c51599148950116850c8fa54ff16b
Vladimir Sibirov <trustmaster@kodigy.com>
2011-06-28 06:13:09 +0000

    Fix for #682

--- plugins/comments/comments.php
+++ plugins/comments/comments.php
@@ -93,7 +93,7 @@ if ($m == 'edit' && $id > 0)
 				$sql2 = $db->query("SELECT * FROM $db_users WHERE user_maingrp=5");
 
 				$email_title = $L['plu_comlive'];
-				$email_body  = $L['User']." ".$usr['name'].", ".$L['plu_comlive3'];
+				$email_body  = $L['User'] . ' ' . preg_replace('#[^\w\p{L}]#u', '', $usr['name']) . ', ' . $L['plu_comlive3'];
 				$email_body .= $url . $sep . 'comments=1#c' . $id . "\n\n";
 
 				while ($adm = $sql2->fetch())
@@ -201,7 +201,7 @@ if ($a == 'send' && $usr['auth_write'])
 		{
 			$sql = $db->query("SELECT * FROM $db_users WHERE user_maingrp=5");
 			$email_title = $L['plu_comlive'];
-			$email_body  = $L['User'] .' ' . $usr['name'] . ', ' . $L['plu_comlive2'];
+			$email_body  = $L['User'] .' ' . preg_replace('#[^\w\p{L}]#u', '', ($usr['id'] == 0 ? $rname : $usr['name'])) . ', ' . $L['plu_comlive2'];
 			$sep = (mb_strpos($url, '?') !== false) ? '&' : '?';
 			$email_body .= $url . $sep . 'comments=1#c' . $id . "\n\n";
 			while ($adm = $sql->fetch())
