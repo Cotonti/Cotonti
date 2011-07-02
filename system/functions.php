6852b815ef357ff0c2d5d3658b75affdde873c48
Vladimir Sibirov <trustmaster@kodigy.com>
2011-07-02 11:20:38 +0000

    Tagged 0.9.3

--- system/functions.php
+++ system/functions.php
@@ -3,7 +3,7 @@
  * Main function library.
  *
  * @package Cotonti
- * @version 0.9.2
+ * @version 0.9.3
  * @author Neocrome, Cotonti Team
  * @copyright Copyright (c) Cotonti Team 2008-2011
  * @license BSD License
@@ -38,8 +38,8 @@ $i = explode(' ', microtime());
 $sys['starttime'] = $i[1] + $i[0];
 
 $cfg['svnrevision'] = '$Rev$'; //DO NOT MODIFY this is set by SVN automatically
-$cfg['version'] = '0.9.2';
-$cfg['dbversion'] = '0.9.2';
+$cfg['version'] = '0.9.3';
+$cfg['dbversion'] = '0.9.3';
 
 // Set default file permissions if not present in config
 if (!isset($cfg['file_perms']))
