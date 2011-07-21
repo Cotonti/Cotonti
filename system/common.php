Cotonti/Cotonti

commit 912456f611aeb8d544bdc70018f1f942ef28c126

Add selectable icons dir

system/common.php
@@ -553,7 +553,7 @@ if (empty($usr['icons']))
 {
 	$usr['icons'] = $cfg['defaulticons'];
 }
-require_once './images/icons/' . $usr['icons'] . '/resources.php';
+require_once $cfg['icons_dir'].'/' . $usr['icons'] . '/resources.php';
 
 $out['copyright'] = "<a href=\"http://www.cotonti.com\">".$L['foo_poweredby']." Cotonti</a>";
 

datas/config-sample.php
@@ -91,6 +91,7 @@ $cfg['system_dir'] = 'system';
 $cfg['thumbs_dir'] = 'datas/thumbs';
 $cfg['themes_dir'] = 'themes';
 $cfg['extrafield_files_dir'] = 'datas/exflds';
+$cfg['icons_dir'] = 'images/icons';
 
 // ========================
 // Directory and file permissions for uploaded files

images/icons/default/resources.php
@@ -14,57 +14,57 @@ defined('COT_CODE') or die('Wrong URL');
  * Arrow Icons
  */
 
-$R['icon_down'] = '<img class="icon" src="images/icons/default/arrow-down.png" alt="" />';
-$R['icon_right'] = '<img class="icon" src="images/icons/default/arrow-right.png" alt="" />';
-$R['icon_left'] = '<img class="icon" src="images/icons/default/arrow-left.png" alt="" />';
-$R['icon_up'] = '<img class="icon" src="images/icons/default/arrow-up.png" alt="" />';
+$R['icon_down'] = '<img class="icon" src="'.$cfg['icons_dir'].'/default/arrow-down.png" alt="" />';
+$R['icon_right'] = '<img class="icon" src="'.$cfg['icons_dir'].'/default/arrow-right.png" alt="" />';
+$R['icon_left'] = '<img class="icon" src="'.$cfg['icons_dir'].'/default/arrow-left.png" alt="" />';
+$R['icon_up'] = '<img class="icon" src="'.$cfg['icons_dir'].'/default/arrow-up.png" alt="" />';
 
-$R['icon_vert_active']['desc'] = '<img class="icon" src="images/icons/default/arrow-up-active.png" alt="" />';
-$R['icon_vert_active']['asc'] = '<img class="icon" src="images/icons/default/arrow-down-active.png" alt="" />';;
+$R['icon_vert_active']['desc'] = '<img class="icon" src="'.$cfg['icons_dir'].'/default/arrow-up-active.png" alt="" />';
+$R['icon_vert_active']['asc'] = '<img class="icon" src="'.$cfg['icons_dir'].'/default/arrow-down-active.png" alt="" />';;
 
-$R['icon_follow'] = '<img class="icon" src="images/icons/default/arrow-follow.png" alt="" />';
-$R['icon_unread'] = '<img class="icon" src="images/icons/default/arrow-unread.png" alt="" />';
+$R['icon_follow'] = '<img class="icon" src="'.$cfg['icons_dir'].'/default/arrow-follow.png" alt="" />';
+$R['icon_unread'] = '<img class="icon" src="'.$cfg['icons_dir'].'/default/arrow-unread.png" alt="" />';
 
 /**
  * Main Icons
  */
 
-$R['icon_admin'] = '<img class="icon" src="images/icons/default/prefs.png" alt="" />';
-$R['icon_comments'] = '<img class="icon" src="images/icons/default/comments.png" alt="" />';
-$R['icon_delete'] = '<img class="icon" src="images/icons/default/delete.png" alt="" />';
-$R['icon_error'] = '<img class="icon" src="images/icons/default/error.png" alt="" />';
-$R['icon_folder'] = '<img class="icon" src="images/icons/default/folder.png" alt="" />';
-$R['icon_forums'] = '<img class="icon" src="images/icons/default/forums.png" alt="" />';
-$R['icon_help'] = '<img class="icon" src="images/icons/default/help.png" alt="" />';
-$R['icon_info'] = '<img class="icon" src="images/icons/default/info.png" alt="" />';
-$R['icon_jumpto'] = '<img class="icon" src="images/icons/default/arrow-jump.png" alt="" />';
-$R['icon_lock'] = '<img class="icon" src="images/icons/default/lock.png" alt="" />';
-$R['icon_message'] = '<img class="icon" src="images/icons/default/message.png" alt="" />';
-$R['icon_news'] = '<img class="icon" src="images/icons/default/news.png" alt="" />';
-$R['icon_online'] = '<img class="icon" src="images/icons/default/online.png" alt="" />';
-$R['icon_page'] = '<img class="icon" src="images/icons/default/page.png" alt="" />';
-$R['icon_pfs'] = '<img class="icon" src="images/icons/default/pfs.png" alt="" />';
-$R['icon_plug'] = '<img class="icon" src="images/icons/default/plug.png" alt="" />';
-$R['icon_polls'] = '<img class="icon" src="images/icons/default/polls.png" alt="" />';
-$R['icon_prefs'] = '<img class="icon" src="images/icons/default/prefs.png" alt="" />';
-$R['icon_reset'] = '<img class="icon" src="images/icons/default/reset.png" alt="" />';
-$R['icon_rights'] = '<img class="icon" src="images/icons/default/rights.png" alt="" />';
-$R['icon_search'] = '<img class="icon" src="images/icons/default/search.png" alt="" />';
-$R['icon_stats'] = '<img class="icon" src="images/icons/default/stats.png" alt="" />';
-$R['icon_stop'] = '<img class="icon" src="images/icons/default/stop.png" alt="" />';
-$R['icon_subfolder'] = '<img class="icon" src="images/icons/default/subfolder.png" alt="" />';
-$R['icon_tags'] = '<img class="icon" src="images/icons/default/tags.png" alt="" />';
-$R['icon_tool'] = '<img class="icon" src="images/icons/default/tool.png" alt="" />';
-$R['icon_undo'] = '<img class="icon" src="images/icons/default/undo.png" alt="" />';
-$R['icon_update'] = '<img class="icon" src="images/icons/default/update.png" alt="" />';
-$R['icon_users'] = '<img class="icon" src="images/icons/default/users.png" alt="" />';
-$R['icon_warning'] = '<img class="icon" src="images/icons/default/warning.png" alt="" />';
+$R['icon_admin'] = '<img class="icon" src="'.$cfg['icons_dir'].'/default/prefs.png" alt="" />';
+$R['icon_comments'] = '<img class="icon" src="'.$cfg['icons_dir'].'/default/comments.png" alt="" />';
+$R['icon_delete'] = '<img class="icon" src="'.$cfg['icons_dir'].'/default/delete.png" alt="" />';
+$R['icon_error'] = '<img class="icon" src="'.$cfg['icons_dir'].'/default/error.png" alt="" />';
+$R['icon_folder'] = '<img class="icon" src="'.$cfg['icons_dir'].'/default/folder.png" alt="" />';
+$R['icon_forums'] = '<img class="icon" src="'.$cfg['icons_dir'].'/default/forums.png" alt="" />';
+$R['icon_help'] = '<img class="icon" src="'.$cfg['icons_dir'].'/default/help.png" alt="" />';
+$R['icon_info'] = '<img class="icon" src="'.$cfg['icons_dir'].'/default/info.png" alt="" />';
+$R['icon_jumpto'] = '<img class="icon" src="'.$cfg['icons_dir'].'/default/arrow-jump.png" alt="" />';
+$R['icon_lock'] = '<img class="icon" src="'.$cfg['icons_dir'].'/default/lock.png" alt="" />';
+$R['icon_message'] = '<img class="icon" src="'.$cfg['icons_dir'].'/default/message.png" alt="" />';
+$R['icon_news'] = '<img class="icon" src="'.$cfg['icons_dir'].'/default/news.png" alt="" />';
+$R['icon_online'] = '<img class="icon" src="'.$cfg['icons_dir'].'/default/online.png" alt="" />';
+$R['icon_page'] = '<img class="icon" src="'.$cfg['icons_dir'].'/default/page.png" alt="" />';
+$R['icon_pfs'] = '<img class="icon" src="'.$cfg['icons_dir'].'/default/pfs.png" alt="" />';
+$R['icon_plug'] = '<img class="icon" src="'.$cfg['icons_dir'].'/default/plug.png" alt="" />';
+$R['icon_polls'] = '<img class="icon" src="'.$cfg['icons_dir'].'/default/polls.png" alt="" />';
+$R['icon_prefs'] = '<img class="icon" src="'.$cfg['icons_dir'].'/default/prefs.png" alt="" />';
+$R['icon_reset'] = '<img class="icon" src="'.$cfg['icons_dir'].'/default/reset.png" alt="" />';
+$R['icon_rights'] = '<img class="icon" src="'.$cfg['icons_dir'].'/default/rights.png" alt="" />';
+$R['icon_search'] = '<img class="icon" src="'.$cfg['icons_dir'].'/default/search.png" alt="" />';
+$R['icon_stats'] = '<img class="icon" src="'.$cfg['icons_dir'].'/default/stats.png" alt="" />';
+$R['icon_stop'] = '<img class="icon" src="'.$cfg['icons_dir'].'/default/stop.png" alt="" />';
+$R['icon_subfolder'] = '<img class="icon" src="'.$cfg['icons_dir'].'/default/subfolder.png" alt="" />';
+$R['icon_tags'] = '<img class="icon" src="'.$cfg['icons_dir'].'/default/tags.png" alt="" />';
+$R['icon_tool'] = '<img class="icon" src="'.$cfg['icons_dir'].'/default/tool.png" alt="" />';
+$R['icon_undo'] = '<img class="icon" src="'.$cfg['icons_dir'].'/default/undo.png" alt="" />';
+$R['icon_update'] = '<img class="icon" src="'.$cfg['icons_dir'].'/default/update.png" alt="" />';
+$R['icon_users'] = '<img class="icon" src="'.$cfg['icons_dir'].'/default/users.png" alt="" />';
+$R['icon_warning'] = '<img class="icon" src="'.$cfg['icons_dir'].'/default/warning.png" alt="" />';
 
 /**
  * Misc
  */
 
-$R['icon_rss'] = '<img class="icon" src="images/icons/default/rss.png" alt="" />';
-$R['icon_twitter'] = '<img class="icon" src="images/icons/default/twitter.png" alt="" />';
+$R['icon_rss'] = '<img class="icon" src="'.$cfg['icons_dir'].'/default/rss.png" alt="" />';
+$R['icon_twitter'] = '<img class="icon" src="'.$cfg['icons_dir'].'/default/twitter.png" alt="" />';
 
 ?>

commit c0164b32ae76e66f860231d989773c51f5869b47

Fix for commit 912456f which broke existing sites and updates

system/common.php
@@ -553,7 +553,15 @@ if (empty($usr['icons']))
 {
 	$usr['icons'] = $cfg['defaulticons'];
 }
-require_once $cfg['icons_dir'].'/' . $usr['icons'] . '/resources.php';
+
+if (file_exists($cfg['icons_dir'].'/' . $usr['icons'] . '/resources.php'))
+{
+	require_once $cfg['icons_dir'].'/' . $usr['icons'] . '/resources.php';
+}
+else
+{
+	require_once './images/icons/' . $cfg['defaulticons'] . '/resources.php';
+}
 
 $out['copyright'] = "<a href=\"http://www.cotonti.com\">".$L['foo_poweredby']." Cotonti</a>";
 