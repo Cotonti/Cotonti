639d4fa4316fa9e2bca15360c7bc1a51b3efc4c0
Vladimir Sibirov <trustmaster@kodigy.com>
2011-08-06 23:32:43 +0400

    Added missing datas/exlfds folder in default package

---
+++ datas/exflds/index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Directory blocker
+ *
+ * @package Cotonti
+ * @version 0.9.0
+ * @author Neocrome, Cotonti Team
+ * @copyright Copyright (c) Cotonti Team 2008-2011
+ * @license BSD
+ */
+
+/* ==== Directory blocker ==== */
+
+?>
