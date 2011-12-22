efb82e63bbe7bc482081387e8c63e11a88807338
Dmitri Beliavski <support@seditio.by>
2011-12-22 20:09:14 +0200

    theme fixes

--- themes/nemesis/nemesis.rc.php
+++ themes/nemesis/nemesis.rc.php
@@ -12,8 +12,7 @@
 defined('COT_CODE') or die('Wrong URL.');
 
 cot_rc_add_file($cfg['themes_dir'].'/nemesis/css/reset.css');
-cot_rc_add_file($cfg['themes_dir'].'/nemesis/css/header.css');
-cot_rc_add_file($cfg['themes_dir'].'/nemesis/css/footer.css');
-cot_rc_add_file($cfg['themes_dir'].'/nemesis/css/default.css');
+cot_rc_add_file($cfg['themes_dir'].'/{PHP.usr.theme}/css/extras.css');
+cot_rc_add_file($cfg['themes_dir'].'/{PHP.usr.theme}/css/default.css');
 
 ?>
