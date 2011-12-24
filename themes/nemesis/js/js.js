03c3c4360f278f89b40b008387ad70d37c5a9260
Dmitri Beliavski <support@seditio.by>
2011-12-24 07:00:31 +0200

    remove test code

--- themes/nemesis/js/js.js
+++ themes/nemesis/js/js.js
@@ -1,8 +1,5 @@
 $().ready(function() {
 
-	$('.block').each(function(){
-		$(this).children('h2').contents().unwrap().wrap('<div class="blockHeader"/>');
-		$(this).children(':gt(0)').wrapAll('<div class="blockWrapper"/>')
-	});
+
 	
 });
