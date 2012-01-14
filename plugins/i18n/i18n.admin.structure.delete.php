556b9b1cbab1b43ec937ad531e1ba096e0ea5fd3
Vladimir Sibirov <trustmaster@kodigy.com>
2012-01-14 14:45:59 +0300

    Added i18n parts to remove page and category tranlsations on page and category removal (fixes #775).

---
+++ plugins/i18n/i18n.admin.structure.delete.php
@@ -0,0 +1,22 @@
+<?php
+/* ====================
+[BEGIN_COT_EXT]
+Hooks=admin.structure.delete.done
+[END_COT_EXT]
+==================== */
+
+/**
+ * Removes category translations
+ *
+ * @package i18n
+ * @version 0.9.4
+ * @author Trustmaster
+ * @copyright Copyright (c) Cotonti Team 2010-2012
+ * @license BSD License
+ */
+
+defined('COT_CODE') or die('Wrong URL');
+
+$db->delete($db_i18n_structure, "istructure_code = ?", array($c));
+
+?>
