c15bd2846bb99a9f0d9d3f75d3b3beb3c8b15f04
Vladimir Sibirov <trustmaster@kodigy.com>
2012-06-18 15:22:37 +0400

    Fixed table prefixes.

--- setup/siena/patch_0.9.11-02.sql
+++ setup/siena/patch_0.9.11-02.sql
@@ -1,7 +1,7 @@
-/* 0.9.11-01 Introduce singular title field for groups */
-ALTER TABLE `cot094_groups` ADD `grp_name` VARCHAR(64) NOT NULL DEFAULT '' AFTER `grp_hidden`;
-UPDATE `cot094_groups` SET `grp_name` = `grp_title`;
-UPDATE `cot094_groups` SET `grp_title` = 'Guest' WHERE `grp_title` = 'Guests';
-UPDATE `cot094_groups` SET `grp_title` = 'Member' WHERE `grp_title` = 'Members';
-UPDATE `cot094_groups` SET `grp_title` = 'Administrator' WHERE `grp_title` = 'Administrators';
-UPDATE `cot094_groups` SET `grp_title` = 'Moderator' WHERE `grp_title` = 'Moderators';
+/* 0.9.11-02 Introduce singular title field for groups */
+ALTER TABLE `cot_groups` ADD `grp_name` VARCHAR(64) NOT NULL DEFAULT '' AFTER `grp_hidden`;
+UPDATE `cot_groups` SET `grp_name` = `grp_title`;
+UPDATE `cot_groups` SET `grp_title` = 'Guest' WHERE `grp_title` = 'Guests';
+UPDATE `cot_groups` SET `grp_title` = 'Member' WHERE `grp_title` = 'Members';
+UPDATE `cot_groups` SET `grp_title` = 'Administrator' WHERE `grp_title` = 'Administrators';
+UPDATE `cot_groups` SET `grp_title` = 'Moderator' WHERE `grp_title` = 'Moderators';
