52c2f49c94cb67f52476eb0ec337faea10b3bdb3
Vladimir Sibirov <trustmaster@kodigy.com>
2012-07-09 23:20:08 +0400

    Invalid file

--- plugins/bbcode/setup/sql_0.9.2.sql
+++
@@ -1,8 +0,0 @@
-/* 0.9.2 Nested quote support */
-
-DELETE FROM `cot_bbcode` WHERE `bbc_name` = 'quote';
-
-INSERT INTO `cot_bbcode` (`bbc_name`, `bbc_mode`, `bbc_pattern`, `bbc_replacement`, `bbc_container`, `bbc_enabled`, `bbc_priority`, `bbc_plug`, `bbc_postrender`) VALUES
-('quote', 'pcre', '\\[quote=(.+?)\\]', '<blockquote><strong>$1:</strong><hr />', 1, 1, 128, '', 0),
-('quote', 'str', '[quote]', '<blockquote>', 1, 1, 128, '', 0),
-('quote', 'str', '[/quote]', '</blockquote>', 1, 1, 128, '', 0);
