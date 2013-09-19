9fed502f240936bc47232410fe659c7c9364a679
Andrew Matsovkin <macik.spb@gmail.com>
2013-09-20 02:24:39 +0400

    Fix improper DB clean on uninstall page module

--- modules/page/setup/page.uninstall.sql
+++ modules/page/setup/page.uninstall.sql
@@ -4,4 +4,5 @@
 
 DROP TABLE IF EXISTS `cot_pages`;
 
-DELETE FROM `cot_structure` WHERE structure_code IN ('articles', 'links', 'events', 'news');
+DELETE FROM `cot_auth` WHERE `auth_code` = 'page';
+DELETE FROM `cot_structure` WHERE `structure_area` = 'page';
