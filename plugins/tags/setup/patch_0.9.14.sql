d580f252701e6b24c38ab4cb1fa6fff6cf071bc6
xerora <tyler@xaez.org>
2013-10-11 12:56:36 -0700

    Fixes #1253

--- plugins/tags/setup/patch_0.9.14.sql
+++
@@ -1,3 +0,0 @@
-/* 0.9.14 update table schema for larger data sets, #981 */
-ALTER TABLE `cot_tag_references` ADD INDEX `tag_area_item` (`tag_area`, `tag_item`);
-ALTER TABLE `cot_tag_references` DROP INDEX `tag_item`;
