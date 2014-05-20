5d2bc2d072172e522d49c7890bde76d8712802b9
Vladimir Sibirov <trustmaster@kodigy.com>
2014-05-20 13:16:55 +0400

    Implemented embedded PHP webserver support #1348.

--- index.php
+++ index.php
@@ -3,11 +3,57 @@
  * Index loader
  *
  * @package Cotonti
- * @version 0.9.4
+ * @version 0.9.17
  * @author Cotonti Team
  * @copyright Copyright (c) Cotonti Team 2008-2014
  * @license BSD
  */
+
+if (php_sapi_name() == 'cli-server')
+{
+	// Embedded PHP webserver routing
+	$REQUEST_FILENAME = mb_substr(explode('?', $_SERVER['REQUEST_URI'])[0], 1);
+	if (file_exists($REQUEST_FILENAME) && !preg_match('#\.php$#', $REQUEST_FILENAME))
+	{
+		// Transfer static file if exists
+		return false;
+	}
+	// Language selector
+	$langs = array_map(function($dir) {
+		return str_replace('lang/', '', $dir);
+	}, glob('lang/??', GLOB_ONLYDIR));
+	if (preg_match('#^('.join('|', $langs).')/(.*)$#', $REQUEST_FILENAME, $mt))
+	{
+		$REQUEST_FILENAME = $mt[2];
+		$_GET['l'] = $mt[1];
+	}
+	// Sitemap shortcut
+	if ($REQUEST_FILENAME === 'sitemap.xml')
+	{
+		$_GET['r'] = 'sitemap';
+	}
+	// Admin area and message are special scripts
+	if (preg_match('#^admin/([a-z0-9]+)#', $REQUEST_FILENAME, $mt))
+	{
+		$_GET['m'] = $mt[1];
+		include 'admin.php';
+		exit;
+	}
+	if (preg_match('#^(admin|login|message)(/|$)#', $REQUEST_FILENAME, $mt))
+	{
+		include $mt[1].'.php';
+		exit;
+	}
+	// PHP files have priority
+	if (preg_match('#\.php$#', $REQUEST_FILENAME))
+	{
+		include $REQUEST_FILENAME;
+		exit;
+	}
+	// All the rest goes through standard rewrite gateway
+	$_GET['rwr'] = $REQUEST_FILENAME;
+	unset($REQUEST_FILENAME, $langs, $mt);
+}
 
 // Redirect to install if config is missing
 if (!file_exists('./datas/config.php'))
