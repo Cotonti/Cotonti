678deed101e381cb10073e6925a730b5feadddf6
Alexey Kalnov <kalnovalexey@yandex.ru>
2014-08-20 02:23:37 +0400

    Revert "quick fix for undefined function issues from 7a0c074b766b0afdd87cb463a7119512aa5cf88d"
    This reverts commit 2410ad17df43a7c45ad0dde1bc698276da887401.

--- modules/users/inc/users.functions.php
+++ modules/users/inc/users.functions.php
@@ -213,3 +213,69 @@ function cot_selectbox_gender($check, $name)
 	}
 	return cot_selectbox($check, $name, $genlist, $titlelist, false);
 }
+
+/**
+ * Fetches user entry from DB
+ *
+ * @param int $uid   User ID
+ * @param bool $cacheitem
+ * @return array
+ */
+function cot_user_data($uid = 0, $cacheitem = true){
+    global $db_users;
+
+    $user = false;
+
+    if(!$uid && cot::$usr['id'] > 0){
+        $uid = cot::$usr['id'];
+        $user = cot::$usr['profile'];
+    }
+    if(!$uid) return null;
+
+    static $u_cache = array();
+
+    if($cacheitem && isset($u_cache[$uid])) {
+        return $u_cache[$uid];
+    }
+
+    if(!$user){
+        if(is_array($uid)){
+            $user = $uid;
+            $uid = $user['user_id'];
+        }else{
+            if($uid > 0 && $uid == cot::$usr['id']){
+                $user = cot::$usr['profile'];
+            }else{
+                $uid = (int)$uid;
+                if(!$uid) return null;
+                $sql = cot::$db->query("SELECT * FROM $db_users WHERE user_id = ? LIMIT 1", $uid);
+                $user = $sql->fetch();
+            }
+        }
+    }
+
+    $cacheitem && $u_cache[$uid] = $user;
+
+    return $user;
+}
+
+/**
+ * user display name
+ * @param array|int $user User Data or User ID
+ * @return array
+ */
+function cot_user_display_name($user){
+
+    if( is_int($user) || ctype_digit($user) ) $user = cot_user_data($user);
+    if(empty($user)) return '';
+
+    if(!empty($user['user_firstname']) || !empty($user['user_lastname']) || !empty($user['user_middlename'])){
+        return trim($user['user_lastname'].' '.$user['user_firstname'].' '.$user['user_middlename']);
+    }
+
+    if(!empty($user['user_first_name']) || !empty($user['user_last_name']) || !empty($user['user_middle_name'])){
+        return trim($user['user_last_name'].' '.$user['user_first_name'].' '.$user['user_middle_name']);
+    }
+
+    return $user['user_name'];
+}
