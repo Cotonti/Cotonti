5d572e0cf9bb51dff080e721dc6b136bd5730df5
Andrew Matsovkin <macik.spb@gmail.com>
2015-12-26 21:24:20 +0300

    I18n Tags search improvment
    + display search results content on current locale
    * search only in main and user selected locale (not show «junk» locales)
    + First display results with user locale

--- plugins/i18n/i18n.tags.search.query.php
+++ plugins/i18n/i18n.tags.search.query.php
@@ -22,7 +22,9 @@ if (is_array($i18n_structure) && count($i18n_structure) > 0)
 	require_once cot_incfile('i18n', 'plug');
 
 	// Join the translation table
-	$join_columns .= ', i18n.*';
+	$join_columns .= ', i18n.*, COALESCE(ipage_text, page_text) as page_text, COALESCE(ipage_desc, page_desc) as page_desc';
 	$join_tables .= "LEFT JOIN $db_i18n_pages AS i18n
 		ON p.page_id = i18n.ipage_id AND r.tag_locale = i18n.ipage_locale";
+	if ($i18n_locale) $join_where .= "AND (r.tag_locale = '' OR r.tag_locale = '$i18n_locale')";
+	if (!$order) $order = 'ORDER BY i18n.ipage_locale DESC, p.page_date DESC';
 }
