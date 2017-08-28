9ac62ca287b41306103eb28c12a59a29fc5d07d4
Jason Booth <kilandor@gmail.com>
2017-08-28 05:23:31 -0500

    Fix for Issue #1580

--- modules/forums/setup/patch_1.1.3.sql
+++ modules/forums/setup/patch_1.1.3.sql
@@ -1,4 +1,4 @@
 /* Update to v 1.1.3 */
-ALTER TABLE cot_forum_posts DROP INDEX `fp_topicid`, ADD UNIQUE fp_topicid_id_idx (fp_topicid, fp_id) USING BTREE;
+ALTER TABLE `cot_forum_posts` DROP INDEX `fp_topicid`, ADD UNIQUE fp_topicid_id_idx (fp_topicid, fp_id) USING BTREE;
 -- ADD index for topicid
-ALTER TABLE cot_forum_posts ADD INDEX fp_topicid_idx (fp_topicid);
+ALTER TABLE `cot_forum_posts` ADD INDEX fp_topicid_idx (fp_topicid);
