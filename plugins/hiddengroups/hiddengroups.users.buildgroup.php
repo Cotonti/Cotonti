0021afc47f943f52c87797ded9aec1b5ba4855bb
Pavlo Tkachenko <dayver2009@gmail.com>
2023-01-01 07:25:32 +0200

    Fix warning's (Plz. test it) + some fix style code
    Fix for report https://www.cotonti.com/ru/forums/45784?m=posts

--- plugins/hiddengroups/hiddengroups.users.buildgroup.php
+++ plugins/hiddengroups/hiddengroups.users.buildgroup.php
@@ -15,13 +15,13 @@ Hooks=users.query
 
 defined('COT_CODE') or die('Wrong URL.');
 
-if($cot_groups[$grpid]['hidden'])
+if (isset($grpid) && $cot_groups[$grpid]['hidden'])
 {
-	if(cot_auth('users', 'a', 'A'))
+	if (cot_auth('users', 'a', 'A'))
 	{
 		return cot_rc_link(cot_url('users', 'gm='.$grpid), $cot_groups[$grpid]['name'].' ('.$L['Hidden'].')');
 	}
-	elseif(!cot_auth('plug', 'hiddengroups', '1'))
+	elseif (!cot_auth('plug', 'hiddengroups', '1'))
 	{
 		return $L['Hidden'];
 	}
