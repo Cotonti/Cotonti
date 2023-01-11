916e2f74d8d02b91b3314915863067852071dd6e
Seditio <support@seditio.by>
2023-01-12 00:42:28 +0300

    1. Remove unused resource strings 2. Add lock icon for locked polls in admin panel

--- modules/polls/inc/polls.resources.php
+++ modules/polls/inc/polls.resources.php
@@ -1,8 +1,2 @@
 <?php
-
-$R['polls_icon_lock'] = '<img src="images/icons/'.$cfg['defaulticons'].'/lock.png" alt="" />';
-$R['polls_icon_unlock'] = '<img src="images/icons/'.$cfg['defaulticons'].'/lock.png" alt="" />';
-$R['polls_icon_delete'] = '<img src="images/icons/'.$cfg['defaulticons'].'/delete.png" alt="" />';
-$R['polls_icon_reset'] = '<img src="images/icons/'.$cfg['defaulticons'].'/reset.png" alt="" />';
-$R['polls_icon_bump'] = '<img src="images/icons/'.$cfg['defaulticons'].'/arrow-up.png" alt="" />';
-$R['polls_icon_open'] = '<img src="images/icons/'.$cfg['defaulticons'].'/arrow-jump.png" alt="" />';
+$R['polls_icon_locked'] = '<img src="images/icons/'.$cfg['defaulticons'].'/lock.png" alt="" class="me-1" />';
