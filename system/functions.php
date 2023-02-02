f5385f2675db37e94eac5d2de4639d9cbe172c57
Seditio <support@seditio.by>
2023-02-02 10:05:06 +0300

    Modify cot_structure_buildpath to use optional extrafield w/backward compatibility (title as default)

--- system/functions.php
+++ system/functions.php
@@ -1883,7 +1883,7 @@ function cot_build_age($birthdate)
  * @return array
  * @see cot_breadcrumbs()
  */
-function cot_structure_buildpath($area, $cat)
+function cot_structure_buildpath($area, $cat, $extrafield = 'title')
 {
 	global $structure;
 	$tmp = array();
@@ -1891,7 +1891,7 @@ function cot_structure_buildpath($area, $cat)
         $pathcodes = explode('.', cot::$structure[$area][$cat]['path']);
         foreach ($pathcodes as $x) {
             if ($x != 'system') {
-                $tmp[] = array(cot_url($area, 'c=' . $x), cot::$structure[$area][$x]['title']);
+                $tmp[] = array(cot_url($area, 'c=' . $x), cot::$structure[$area][$x][$extrafield]);
             }
         }
     }
