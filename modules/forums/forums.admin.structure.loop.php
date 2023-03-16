083669e807156bdf69a888a570f0e106751a6447
Alexey Kalnov <kalnovalexey@yandex.ru>
2023-03-16 09:31:29 +0300

    #1680 Fix 2

--- modules/forums/forums.admin.structure.loop.php
+++ modules/forums/forums.admin.structure.loop.php
@@ -13,14 +13,15 @@ Hooks=admin.structure.loop
  * @license https://github.com/Cotonti/Cotonti/blob/master/License.txt
  *
  * @var XTemplate $t
+ * @var string $n Extension code
  * @var string $structureCode
- * @var array $row
+ * @var array $row Structure row
  * @var int $structureLevel Category nesting level
  */
 
 defined('COT_CODE') or die('Wrong URL');
 
-if ($structureLevel > 1) {
+if ($n == 'forums' && $structureLevel > 1) {
     $t->assign([
         'ADMIN_STRUCTURE_JUMPTO_URL' => cot_url('forums', ['m' => 'topics', 's' =>  $structureCode]),
     ]);
