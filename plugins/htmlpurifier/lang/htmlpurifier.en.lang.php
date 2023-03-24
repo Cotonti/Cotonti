251cea5bf7a6a0169384de14871ff7e3d8993b77
Seditio <support@seditio.by>
2023-03-24 13:15:56 +0300

    Minor lang fix

--- plugins/htmlpurifier/lang/htmlpurifier.en.lang.php
+++ plugins/htmlpurifier/lang/htmlpurifier.en.lang.php
@@ -15,7 +15,7 @@ $L['cfg_rel2abs'] = 'Relative links to Absolute';
 $L['cfg_rel2abs_hint'] = 'If enabled, will turn all relative URLs to absolute, based on your host and domain';
 $L['cfg_tidylevel'] = 'Tidy Level';
 $L['cfg_tidylevel_hint'] = 'Sets how intensively HTML Purifier will fix markup mistakes';
-$L['cfg_videoiframe'] = 'Allow users to insert video iframes from Youtube/Vimeo';
+$L['cfg_videoiframe'] = 'Allow users to embed video iframes from Youtube/Vimeo';
 $L['cfg_videoiframe_hint'] = '(only for registred users)';
 
 $L['info_desc'] = 'Makes sure submitted HTML content is valid, clean and secure';
