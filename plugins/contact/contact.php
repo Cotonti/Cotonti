801e0d0fb3a1f272deaa29a3dd2bab5136797556
Alexey Kalnov <kalnovalexey@yandex.ru>
2023-08-22 11:25:36 +0300

    Remove obsolete code

--- plugins/contact/contact.php
+++ plugins/contact/contact.php
@@ -14,34 +14,6 @@
  */
 defined('COT_CODE') or die('Wrong URL');
 
-if (!empty($cot_captcha)) {
-	if (!function_exists('cot_captcha_generate')) {
-
-		function cot_captcha_generate($func_index = 0) {
-			global $cot_captcha;
-			if (!empty($cot_captcha[$func_index])) {
-				$captcha = $cot_captcha[$func_index] . '_generate';
-				return $captcha();
-			}
-			return false;
-		}
-
-	}
-
-	if (!function_exists('cot_captcha_validate')) {
-
-		function cot_captcha_validate($verify = 0, $func_index = 0) {
-			global $cot_captcha;
-			if (!empty($cot_captcha[$func_index])) {
-				$captcha = $cot_captcha[$func_index] . '_validate';
-				return $captcha($verify);
-			}
-			return false;
-		}
-
-	}
-}
-
 $tplfile = cot_import('tpl', 'G', 'TXT');
 $mskin = cot_tplfile(array('contact', $tplfile), 'plug');
 $t = new XTemplate($mskin);
