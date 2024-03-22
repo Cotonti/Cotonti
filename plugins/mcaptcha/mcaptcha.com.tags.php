2ea54f457be0794f04d52b36c7d2a7d73e11eda7
Alexey Kalnov <kalnovalexey@yandex.ru>
2024-03-22 07:46:04 +0300

    Some deprecated things in legacy mode - 2

--- plugins/mcaptcha/mcaptcha.com.tags.php
+++ plugins/mcaptcha/mcaptcha.com.tags.php
@@ -19,15 +19,16 @@ Tags=comments.tpl: {COMMENTS_FORM_VERIFY_IMG}, {COMMENTS_FORM_VERIFY_INPUT}
 defined('COT_CODE') or die("Wrong URL.");
 
 if (Cot::$usr['id'] === 0 && Cot::$cfg['captchamain'] === 'mcaptcha') {
+    $captcha = cot_captcha_generate();
 	$t->assign([
-		'COMMENTS_FORM_VERIFY_IMG' => cot_captcha_generate(),
+		'COMMENTS_FORM_VERIFY_IMG' => $captcha,
 		'COMMENTS_FORM_VERIFY_INPUT' => cot_inputbox('text', 'rverify', '', 'size="10" maxlength="20"'),
 	]);
 
     if (isset(Cot::$cfg['legacyMode']) && Cot::$cfg['legacyMode']) {
         // @deprecated in 0.9.24
         $t->assign([
-            'COMMENTS_FORM_VERIFYIMG' => cot_captcha_generate(),
+            'COMMENTS_FORM_VERIFYIMG' => $captcha,
             'COMMENTS_FORM_VERIFY' => cot_inputbox('text', 'rverify', '', 'size="10" maxlength="20"'),
         ]);
     }
