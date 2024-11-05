1ba8a43fe73ffdf5147fa3ebf27732a70062fbec
Ali Çömez | Slaweally <sys@rootali.net>
2024-11-05 14:40:00 +0300

    Update and rename users.en.lang.php to users.tr.lang.php

---
+++ modules/users/lang/users.tr.lang.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Kullanıcı Modülü için Türkçe Dil Dosyası
+ *
+ * @package Users
+ * @copyright (c) Cotonti Ekibi
+ * @license https://github.com/Cotonti/Cotonti/blob/master/License.txt
+ */
+
+defined('COT_CODE') or die('Yanlış URL.');
+
+$L['info_desc'] = 'Kullanıcı yönetimi, kayıt, listeleme ve profiller';
+
+/**
+ * Modül Yapılandırması
+ */
+$L['cfg_disablereg'] = 'Kayıt işlemini devre dışı bırak';
+$L['cfg_disablereg_hint'] = 'Kullanıcıların yeni hesap kaydetmelerini engelle';
+$L['cfg_maxusersperpage'] = 'Kullanıcı listesinde maksimum satır';
+$L['cfg_maxusersperpage_hint'] = '';
+$L['cfg_regnoactivation'] = 'Yeni kullanıcılar için e-posta kontrolünü atla';
+$L['cfg_regnoactivation_hint'] = 'Güvenlik nedenleriyle "Hayır" önerilir';
+$L['cfg_register_auto_login'] = 'Kayıttan sonra otomatik giriş yap';
+$L['cfg_register_auto_login_hint'] = 'İlgili seçenek etkinse, pasif kullanıcılar yetkilendirilecektir.';
+$L['cfg_inactive_login'] = 'Pasif kullanıcıların giriş yapmasına izin ver';
+$L['cfg_inactive_login_hint'] = '&laquo;Pasif&raquo; grubundaki kullanıcıların giriş yapmasına izin ver';
+$L['cfg_regrequireadmin'] = 'Yöneticiler yeni hesapları onaylamalı';
+$L['cfg_regrequireadmin_hint'] = '';
+$L['cfg_user_email_noprotection'] = 'E-posta değişikliğinde şifre korumasını devre dışı bırak';
+$L['cfg_user_email_noprotection_hint'] = 'Güvenlik nedenleriyle "Hayır" önerilir';
+$L['cfg_useremailchange'] = 'Kullanıcıların e-posta adreslerini değiştirmelerine izin ver';
+$L['cfg_useremailchange_hint'] = 'Güvenlik nedenleriyle "Hayır" önerilir';
+$L['cfg_usertextmax'] = 'Kullanıcı imzası için maksimum uzunluk';
+$L['cfg_usertextmax_hint'] = '';
+$L['cfg_usertextimg'] = 'Kullanıcı imzasında resim ve HTML\'e izin ver';
+$L['cfg_usertextimg_hint'] = 'Güvenlik nedenleriyle "Hayır" önerilir';
+
+/**
+ * Kullanıcı profili & düzenleme
+ */
+$L['Password_updated'] = 'Şifre değiştirildi';
+$L['Profile_updated'] = 'Profil ayarları kaydedildi';
+$L['User_data_updated'] = 'Kullanıcı verileri güncellendi';
+$L['Filter_search'] = 'Filtre araması';
+$L['Username_search'] = 'Kullanıcı adı araması';
+$L['Not_indicated'] = 'Seçilmedi';
+
+$L['users_meta_title'] = 'Kullanıcı hesapları listesi';
+$L['users_meta_desc'] = 'Kayıtlı kullanıcı hesaplarının listesi';
