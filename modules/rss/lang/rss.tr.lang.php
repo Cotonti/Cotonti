d7f926599500292ed2d4ea224d9e1b85871338d9
Ali Çömez | Slaweally <sys@rootali.net>
2024-11-05 14:41:35 +0300

    Update and rename rss.en.lang.php to rss.tr.lang.php

---
+++ modules/rss/lang/rss.tr.lang.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * RSS Modülü için Türkçe Dil Dosyası (rss.tr.lang.php)
+ *
+ * @package RSS
+ * @copyright (c) Cotonti Ekibi
+ * @license https://github.com/Cotonti/Cotonti/blob/master/License.txt
+ */
+
+defined('COT_CODE') or die('Yanlış URL.');
+
+/**
+ * Modül Bölümü
+ */
+
+$L['cfg_rss_timetolive'] = 'RSS önbelleğini her N saniyede yenile';
+$L['cfg_rss_timetolive_hint'] = ' ';
+$L['cfg_rss_maxitems'] = 'RSS akışındaki maksimum öğe sayısı';
+$L['cfg_rss_maxitems_hint'] = ' ';
+$L['cfg_rss_charset'] = 'RSS karakter seti';
+$L['cfg_rss_charset_hint'] = ' ';
+$L['cfg_rss_pagemaxsymbols'] = 'Sayfalar. Açıklamayı N sembolden uzun kes';
+$L['cfg_rss_pagemaxsymbols_hint'] = 'Varsayılan olarak devre dışıdır';
+$L['cfg_rss_postmaxsymbols'] = 'Mesajlar. Açıklamayı N sembolden uzun kes';
+$L['cfg_rss_postmaxsymbols_hint'] = 'Varsayılan olarak devre dışıdır';
+
+$L['info_desc'] = 'Site içeriği ile RSS/Atom beslemeleri için destek sağlar';
+
+/**
+ * Ana
+ */
+
+$L['rss_allforums_item_title'] = 'Son forum mesajları';
+$L['rss_title'] = 'RSS';
+$L['rss_topic_item_desc'] = 'Son konu mesajları';
+
+/**
+ * Hatalar
+ */
+//$L['rss_error_private'] = 'Bu konu özeldir';
+//$L['rss_error_guests'] = 'Misafirler için okunamaz';
