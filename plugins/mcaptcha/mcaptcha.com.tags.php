c10b88ff95e689cd8de56694fc0e32fc7f5f6be3
Ali Çömez | Slaweally <sys@rootali.net>
2025-03-08 06:51:55 +0300

    Update mcaptcha.com.tags.php
    Use global variable ($usr, $cfg) instead of static (Cot::$usr, Cot::$cfg).

--- plugins/mcaptcha/mcaptcha.com.tags.php
+++ plugins/mcaptcha/mcaptcha.com.tags.php
@@ -2,12 +2,12 @@
 /* ====================
 [BEGIN_COT_EXT]
 Hooks=comments.newcomment.tags
-Tags=comments.tpl: {COMMENTS_FORM_VERIFY_IMG}, {COMMENTS_FORM_VERIFY_INPUT}
+Tags=comments.tpl:{COMMENTS_FORM_VERIFY_IMG};comments.tpl:{COMMENTS_FORM_VERIFY_INPUT}
 [END_COT_EXT]
 ==================== */
 
 /**
- * mCAPTCHA functions
+ * mCAPTCHA tags for comments form
  *
  * @package MathCaptcha
  * @copyright (c) Cotonti Team
@@ -18,15 +18,15 @@ Tags=comments.tpl: {COMMENTS_FORM_VERIFY_IMG}, {COMMENTS_FORM_VERIFY_INPUT}
 
 defined('COT_CODE') or die("Wrong URL.");
 
-if (Cot::$usr['id'] === 0 && Cot::$cfg['captchamain'] === 'mcaptcha') {
+if ($usr['id'] === 0 && $cfg['captchamain'] === 'mcaptcha') {
     $captchaTags = cot_generateCaptchaTags(null, 'rverify', 'COMMENTS_FORM_');
     $t->assign($captchaTags);
 
-    if (isset(Cot::$cfg['legacyMode']) && Cot::$cfg['legacyMode']) {
+    if (isset($cfg['legacyMode']) && $cfg['legacyMode']) {
         // @deprecated in 0.9.24
         $t->assign([
             'COMMENTS_FORM_VERIFYIMG' => $captchaTags['COMMENTS_FORM_VERIFY_IMG'],
-            'COMMENTS_FORM_VERIFY' => cot_inputbox('text', 'rverify', '', 'size="10" maxlength="20"'),
+            'COMMENTS_FORM_VERIFYINPUT' => cot_inputbox('text', 'rverify', '', 'size="10" maxlength="20"'),
         ]);
     }
 }
