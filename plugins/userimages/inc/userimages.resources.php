9bf379f4b4ba2395ac8f89b4037153e7cae42051
seditio <support@seditio.by>
2025-12-19 17:07:33 +0300

    Fix lang-strings for the userimages plugin

--- plugins/userimages/inc/userimages.resources.php
+++ plugins/userimages/inc/userimages.resources.php
@@ -2,11 +2,10 @@
 
 defined('COT_CODE') or die('Wrong URL');
 
-$R['userimg_existing'] = '<div class="userimg_existing"><img src="{$url_file}" alt="" /><br /><a href="{$url_delete}">'.$L['Delete'].'</a></div>';
+$R['userimg_existing'] = '<div class="userimg_existing"><img src="{$url_file}" alt="" /><br /><a href="{$url_delete}">' . Cot::$L['Delete'] . '</a></div>';
 $R['userimg_selectfile'] = '{$form_input}';
 $R['userimg_html'] = '<div class="userimg_{$code}">{$existing}{$selectfile}</div>';
-$R['userimg_remove'] = '<a href="{$url}" class="button">'.$L['Delete'].'</a>';
+$R['userimg_remove'] = '<a href="{$url}" class="button">' . Cot::$L['Delete'] . '</a>';
 $R['userimg_img'] = '<img src="{$src}" alt="{$alt}" class="userimg {$class}" />';
 
-$R['userimg_default_avatar'] = '<img src="' . Cot::$R['users_defaultAvatarSrc']
-    . '" alt="' . Cot::$L['Avatar'] . '" class="avatar" />';
+$R['userimg_default_avatar'] = '<img src="' . Cot::$R['users_defaultAvatarSrc'] . '" alt="' . Cot::$L['Avatar'] . '" class="avatar" />';
